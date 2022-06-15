abbaf84712ab0ccb3b41f3d12db8fa83e9b3971c
Grzegorz Pabian <greg@intuita.io>
2022-06-15 15:04:25 +0200

    apply git diff a b | git apply for all ast change support

--- src/getAstChangedSourceFileText.ts
+++ src/getAstChangedSourceFileText.ts
@@ -1,57 +1,208 @@
-import { Node, Project } from "ts-morph";
-import { AstChange, AstChangeKind } from "./getAstChanges";
+import {Node, Project} from "ts-morph";
+import {AstChange, AstChangeKind} from "./getAstChanges";
+
+export class AstChangeApplier {
+    protected _project: Project;
+
+    public constructor(
+        protected _astChanges: ReadonlyArray<AstChange>,
+        sourceFiles: ReadonlyArray<[string, string]>,
+    ) {
+        const project = new Project({
+            useInMemoryFileSystem: true,
+        });
+
+        sourceFiles.map(
+            ([filePath, sourceFileText]) => {
+                project.createSourceFile(
+                    filePath,
+                    sourceFileText,
+                );
+            }
+        );
+
+        this._project = project;
+    }
+
+    public applyChanges(): ReadonlyArray<[string, string]> {
+        this._astChanges.forEach((astChange) => {
+            switch(astChange.kind) {
+                case AstChangeKind.ARROW_FUNCTION_PARAMETER_DELETED:
+                    this._applyArrowFunctionParameterDeletedChange(astChange);
+                    return;
+                case AstChangeKind.FUNCTION_PARAMETER_DELETED:
+                    this._applyFunctionParameterDeletedChange(astChange);
+                    return;
+                case AstChangeKind.CLASS_METHOD_PARAMETER_DELETED:
+                    this._applyClassMethodParameterDeletedChange(astChange);
+                    return;
+            }
+        });
+
+        return this._project.getSourceFiles().map(
+            (sourceFile) => [
+                sourceFile.getFilePath(),
+                sourceFile.getFullText()
+            ]
+        )
+    }
+
+    protected _applyClassMethodParameterDeletedChange(
+        astChange: AstChange & { kind: AstChangeKind.CLASS_METHOD_PARAMETER_DELETED },
+    ) {
+        const index = astChange.parameters.findIndex(p => p === astChange.parameter);
+
+        if (index === -1) {
+            return;
+        }
+
+        const sourceFile = this._project.getSourceFile(astChange.filePath)
+
+        if (!sourceFile) {
+            return;
+        }
+
+        const classDeclaration = sourceFile.getClass(astChange.className);
+
+        if (!classDeclaration) {
+            return;
+        }
+
+        const methodDeclaration = classDeclaration.getMethod(astChange.methodName);
+
+        if (!methodDeclaration) {
+            return;
+        }
+
+        methodDeclaration
+            .findReferences()
+            .flatMap((referencedSymbol) => referencedSymbol.getReferences())
+            .forEach(
+                (reference) => {
+                    const parentNode = reference.getNode().getParent();
+
+                    if (!Node.isPropertyAccessExpression(parentNode)) {
+                        return;
+                    }
+
+                    const callExpression = parentNode.getParent();
+
+                    if (!Node.isCallExpression(callExpression)) {
+                        return;
+                    }
+
+                    const argument = callExpression.getArguments()[index];
+
+                    if (!argument) {
+                        console.log('no argument')
+                        return;
+                    }
+
+                    callExpression.removeArgument(argument);
+                }
+            );
+    }
+
+    protected _applyFunctionParameterDeletedChange(
+        astChange: AstChange & { kind: AstChangeKind.FUNCTION_PARAMETER_DELETED },
+    ) {
+        const index = astChange.parameters.findIndex(p => p === astChange.parameter);
+
+        if (index === -1) {
+            return;
+        }
+
+        const sourceFile = this._project.getSourceFile(astChange.filePath)
+
+        if (!sourceFile) {
+            return;
+        }
+
+        const functionDeclaration = sourceFile.getFunction(astChange.functionName);
+
+        if (!functionDeclaration) {
+            return;
+        }
+
+        functionDeclaration
+            .findReferences()
+            .flatMap((referencedSymbol) => referencedSymbol.getReferences())
+            .forEach(
+                (reference) => {
+                    const parentNode = reference.getNode().getParent();
+
+                    if (!Node.isCallExpression(parentNode)) {
+                        return;
+                    }
+
+                    const argument = parentNode.getArguments()[index];
+
+                    if (!argument) {
+                        return;
+                    }
+
+                    parentNode.removeArgument(argument);
+                }
+            );
+    }
+
+    protected _applyArrowFunctionParameterDeletedChange(
+        astChange: AstChange & { kind: AstChangeKind.ARROW_FUNCTION_PARAMETER_DELETED },
+    ) {
+        const index = astChange.parameters.findIndex(p => p === astChange.parameter);
+
+        if (index === -1) {
+            return;
+        }
+
+        const sourceFile = this._project.getSourceFile(astChange.filePath)
+
+        if (!sourceFile) {
+            return;
+        }
+
+        const variableDeclaration = sourceFile.getVariableDeclaration(astChange.arrowFunctionName);
+
+        if (!variableDeclaration) {
+            return;
+        }
+
+        variableDeclaration
+            .findReferences()
+            .flatMap((referencedSymbol) => referencedSymbol.getReferences())
+            .forEach(
+                (reference) => {
+                    const parentNode = reference.getNode().getParent();
+
+                    if (!Node.isCallExpression(parentNode)) {
+                        return;
+                    }
+
+                    const argument = parentNode.getArguments()[index];
+
+                    if (!argument) {
+                        return;
+                    }
+
+                    parentNode.removeArgument(argument);
+                }
+            );
+    }
+
+}
 
 export const getAstChangedSourceFileText = (
     astChanges: ReadonlyArray<AstChange>,
     newSourceFileText: string,
 ) => {
-    const project = new Project({ useInMemoryFileSystem: true });
+    const applier = new AstChangeApplier(
+        astChanges,
+        [
+            ['index.ts', newSourceFileText]
+        ],
+    );
 
-    const sourceFile = project.createSourceFile(
-        'index.ts',
-        newSourceFileText,
-    )
+    const x = applier.applyChanges();
 
-    astChanges.forEach(
-        (astChange) => {
-            if (astChange.kind !== AstChangeKind.ARROW_FUNCTION_PARAMETER_DELETED) {
-                return;
-            }
-
-            const index = astChange.parameters.findIndex(p => p === astChange.parameter);
-
-            if (index === -1) {
-                return;
-            }
-
-            const variableDeclaration = sourceFile.getVariableDeclaration(astChange.arrowFunctionName);
-
-            if (!variableDeclaration) {
-                return;
-            }
-
-            variableDeclaration
-                .findReferences()
-                .flatMap((referencedSymbol) => referencedSymbol.getReferences())
-                .forEach(
-                    (reference) => {
-                        const parentNode = reference.getNode().getParent()
-
-                        if (!Node.isCallExpression(parentNode)) {
-                            return;
-                        }
-
-                        const argument = parentNode.getArguments()[index];
-
-                        if (!argument) {
-                            return;
-                        }
-
-                        parentNode.removeArgument(argument);
-                    }
-                )
-        }
-    )
-
-    return sourceFile.getFullText();
+    return x[0];
 }
--- src/test/getAstChangedSourceFileText.test.ts
+++ src/test/getAstChangedSourceFileText.test.ts
@@ -1,45 +1,86 @@
 import { assert } from "chai"
-import { getAstChangedSourceFileText } from "../getAstChangedSourceFileText"
+import {AstChangeApplier, getAstChangedSourceFileText} from "../getAstChangedSourceFileText"
 import { getAstChanges } from "../getAstChanges"
 
 const oldSourceFileText = 
 `
-const x = (a: number, b: string, c: boolean) => {}
+export const x = (a: number, b: string, c: boolean) => {}
+
+export function f(fa: number, fb: string, fc: boolean) {}
+
+export class C {
+    public cm(cma: number, cmb: string, cmc: boolean) {
+    
+    }
+}
 
 const y = () => {
     x(1, "2", false)
+    f(1, "2", false)
+    
+    const c = new C();
+    c.cm(1, "2", true);
 }
 `
 
 const newSourceFileText = 
 `
-const x = (a: number, c: boolean) => {}
+export const x = (a: number, c: boolean) => {}
+
+export function f(fb: string, fc: boolean) {}
+
+export class C {
+    public cm(cma: number, cmb: string) {
+    
+    }
+}
 
 const y = () => {
     x(1, "2", false)
+    f(1, "2", false)
+    
+    const c = new C();
+    c.cm(1, "2", true);
 }
 `
 
-const astChangedSourceFileText = `
-const x = (a: number, c: boolean) => {}
-
-const y = () => {
-    x(1, false)
-}
+const b =
 `
+import { x, f, C} from './a'
+
+const z = () => {
+    x(4, "8", true);
+    f(2, "4", false);
+    
+    const c = new C();
+    c.cm(1, "2", false);
+}
+`;
 
 describe('getAstChangedSourceFileText', () => {
     it('should remove the second argument', () => {
         const astChanges = getAstChanges(
+            'a.ts',
             oldSourceFileText,
             newSourceFileText,
-        )
+        );
 
-        const string = getAstChangedSourceFileText(
+        const applier = new AstChangeApplier(
             astChanges,
-            newSourceFileText,
-        )
+            [
+                ['a.ts', newSourceFileText],
+                ['b.ts', b],
+            ],
+        );
 
-        assert.deepEqual(string, astChangedSourceFileText)
+        const sourceFiles = applier.applyChanges();
+
+        console.log(sourceFiles);
+
+        assert.equal(sourceFiles.length, 2);
+        assert.isTrue(sourceFiles[0]![1]!.includes('x(1, false)'));
+        assert.isTrue(sourceFiles[0]![1]!.includes('f("2", false)'));
+        assert.isTrue(sourceFiles[1]![1]!.includes('x(4, true)'));
+        assert.isTrue(sourceFiles[1]![1]!.includes('f("4", false)'));
     })
 })
