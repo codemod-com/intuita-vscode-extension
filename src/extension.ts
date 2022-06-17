e9d0e8da6a3e6f94627f303018bbb804b41785a9
Grzegorz Pabian <100318192+gregintuitaio@users.noreply.github.com>
2022-06-17 19:35:29 +0200

    Load TS-Morphing during workspace opening  (#6)
    * support watched projects
    
    * load files only when mutex is active
    
    * make the console log better

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
 import { join } from 'path';
 import * as vscode from 'vscode';
 import { getAstChanges } from './getAstChanges';
-import {AstChangeApplier} from "./getAstChangedSourceFileText";
-import {Project} from "ts-morph";
+import { AstChangeApplier } from "./getAstChangedSourceFileText";
+import { Project } from "ts-morph";
+import { watchProject } from './watchedProject';
 
 export async function activate(context: vscode.ExtensionContext) {
 	console.log('Activated the Intuita VSCode Extension')
@@ -15,18 +16,42 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	const openedTextDocuments = new Map<string, string>();
 	const changedTextDocuments = new Map<string, string>();
+	const projects: Project[] = [];
+
+	vscode.workspace.workspaceFolders?.forEach(
+		(workspaceFolder) => {
+			const { fsPath } = workspaceFolder.uri;
+
+			const project = new Project({
+				tsConfigFilePath: join(
+					fsPath,
+					'tsconfig.json'
+				),
+			})
+
+			watchProject(project);
+
+			projects.push(project);
+		}
+	)
+
+	const addTextDocument = (textDocument: vscode.TextDocument) => {
+		const fileName = textDocument.fileName.replace('.git', '');
+
+		console.log(`The document "${fileName}" has been opened.`);
+
+		const text = textDocument.getText();
+
+		openedTextDocuments.set(fileName, text)
+		changedTextDocuments.set(fileName, text);
+	}
+
+	vscode.workspace.textDocuments.map(
+		addTextDocument,
+	)
 
 	vscode.workspace.onDidOpenTextDocument(
-		(document) => {
-			const fileName = document.fileName.replace('.git', '');
-
-			console.log(`The document "${fileName}" has been opened.`);
-
-			const text = document.getText();
-
-			openedTextDocuments.set(fileName, text)
-			changedTextDocuments.set(fileName, text);
-		}
+		addTextDocument,
 	)
 
 	vscode.workspace.onDidCloseTextDocument(
@@ -38,57 +63,82 @@ export async function activate(context: vscode.ExtensionContext) {
 		}
 	)
 
-	vscode.workspace.onDidSaveTextDocument(
-		(document)=> {
-			const { fileName } = document;
+	const mutexifyCallback = (
+		callback: (textDocument: vscode.TextDocument) => void,
+	) => (textDocument: vscode.TextDocument) => {
+		let allow = true;
 
-			console.log(`The document "${fileName}" has been saved.`);
-
-			const newText = document.getText();
-
-			changedTextDocuments.set(
-				fileName,
-				newText,
-			);
-
-			const oldText = openedTextDocuments.get(fileName);
-
-			if (!oldText) {
-				console.log(`No text for "${fileName}" could have been extracted.`);
+		try {
+			if (!allow) {
+				console.log('Cannot allow the incoming event in');
 				return;
 			}
-
-			const astChanges = getAstChanges(
-				fileName,
-				oldText,
-				newText,
-			);
-
-			if (!astChanges) {
-				console.log(`No AST changes for ${fileName} have been recognized.`)
-				return;
-			}
-
-			const project = new Project({
-				tsConfigFilePath: join(
-					vscode.workspace.workspaceFolders?.[0]?.uri.fsPath ?? '',
-					'tsconfig.json'
-				),
-			})
 			
-			const astChangeApplier = new AstChangeApplier(
-				project,
-				astChanges,
-			)
+			allow = false;
 
-			const changes = astChangeApplier.applyChanges()
-
-			if (changes.length) {
-				vscode.window.showInformationMessage(
-					`Applied changes in ${changes.length} file(s).`
-				);
-			}
+			callback(textDocument);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			allow = true;
 		}
+	}
+
+	vscode.workspace.onDidSaveTextDocument(
+		mutexifyCallback(
+			(document) => {
+				const { fileName } = document;
+
+				console.log(`The document "${fileName}" has been saved.`);
+	
+				const newText = document.getText();
+	
+				changedTextDocuments.set(
+					fileName,
+					newText,
+				);
+	
+				const oldText = openedTextDocuments.get(fileName);
+	
+				if (!oldText) {
+					console.log(`No text for "${fileName}" could have been extracted.`);
+					return;
+				}
+	
+				const astChanges = getAstChanges(
+					fileName,
+					oldText,
+					newText,
+				);
+	
+				if (!astChanges) {
+					console.log(`No AST changes for ${fileName} have been recognized.`)
+					return;
+				}
+	
+				const project = projects.find(
+					(_project) => _project.getSourceFile(fileName) !== undefined,
+				)
+	
+				if (!project) {
+					console.log(`Did not find a project for the file name ${fileName}`)
+					return;
+				}
+				
+				const astChangeApplier = new AstChangeApplier(
+					project,
+					astChanges,
+				)
+	
+				const changes = astChangeApplier.applyChanges()
+	
+				if (changes.length) {
+					vscode.window.showInformationMessage(
+						`Applied changes in ${changes.length} file(s).`
+					);
+				}
+			}
+		)
 	)
 }
 
