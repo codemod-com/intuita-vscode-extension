3fdecac1c13c4e81bc6d3056c0e86cf51074835c
Grzegorz Pabian <greg@intuita.io>
2022-06-20 13:01:57 +0200

    expect R | null to work as the return type

--- src/test/moveStaticMethodsOutOfClass/sourceFileText3.ts
+++ src/test/moveStaticMethodsOutOfClass/sourceFileText3.ts
@@ -1,4 +1,4 @@
-export function a<T extends boolean, U extends number, V extends string>(p1: T, p2: U, p3: V): null {
+export function a<T extends boolean, U extends number, V extends string, R>(p1: T, p2: U, p3: V): R | null {
     return null;
 }
 
