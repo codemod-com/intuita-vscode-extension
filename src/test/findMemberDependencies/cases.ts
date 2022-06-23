a3ba11ca9a1d4a2baf8c513cc5ecc6c489ae3ade
Grzegorz Pabian <greg@intuita.io>
2022-06-23 13:17:41 +0200

    make 5 cases for member dependencies

--- src/test/findMemberDependencies/cases.ts
+++ src/test/findMemberDependencies/cases.ts
@@ -1,5 +1,22 @@
 /** old-001 **/
 {
+    class A {}
+}
+/** new-001 **/
+
+/** old-002 **/
+{
+    class A {
+        public ma() {
+
+        }
+    }
+}
+/** new-002 **/
+
+
+/** old-003 **/
+{
     class A {
         private readonly pa = 1;
 
@@ -8,4 +25,32 @@
         }
     }
 }
-/** new-001 **/
+/** new-003 **/
+
+/** old-004 **/
+{
+    class A {
+        private pa = 1;
+
+        public ma() {
+            return this.pa;
+        }
+    }
+}
+/** new-004 **/
+
+/** old-005 **/
+{
+    class A {
+        private readonly pa = 1;
+        private readonly pb = 2;
+        private readonly pc = 3;
+
+        public ma() {
+            console.log(this.pa);
+            console.log(this.pb);
+            console.log(this.pc);
+        }
+    }
+}
+/** new-005 **/
--- src/test/findMemberDependencies/index.test.ts
+++ src/test/findMemberDependencies/index.test.ts
@@ -5,7 +5,7 @@ import {getClassInstanceProperties} from "../../tsMorphAdapter/getClassInstanceP
 import {getMethodMap} from "../../intuitaExtension/getMethodMap";
 import {Mutability} from "../../intuitaExtension/mutability";
 
-describe('find member dependencies', () => {
+describe.only('find member dependencies', () => {
     const caseMap = buildCaseMap(
         __dirname,
     );
@@ -33,9 +33,40 @@ describe('find member dependencies', () => {
 
             const methodMap = getMethodMap(properties);
 
-            assert.equal(methodMap.size, 1);
-            assert.equal(methodMap.get('ma')?.mutability, Mutability.READING_READONLY)
-            assert.deepEqual(methodMap.get('ma')?.propertyNames, ['pa']);
+            switch(caseNumber) {
+                case 1: {
+                    assert.equal(methodMap.size, 0);
+                    return;
+                }
+
+                case 2: {
+                    assert.equal(methodMap.size, 1);
+                    assert.equal(methodMap.get('ma')?.mutability, Mutability.READING_READONLY)
+                    assert.deepEqual(methodMap.get('ma')?.propertyNames, ['']);
+                    return;
+                }
+
+                case 3: {
+                    assert.equal(methodMap.size, 1);
+                    assert.equal(methodMap.get('ma')?.mutability, Mutability.READING_READONLY)
+                    assert.deepEqual(methodMap.get('ma')?.propertyNames, ['pa']);
+                    return;
+                }
+
+                case 4: {
+                    assert.equal(methodMap.size, 1);
+                    assert.equal(methodMap.get('ma')?.mutability, Mutability.WRITING_WRITABLE)
+                    assert.deepEqual(methodMap.get('ma')?.propertyNames, ['pa']);
+                    return;
+                }
+
+                case 5: {
+                    assert.equal(methodMap.size, 1);
+                    assert.equal(methodMap.get('ma')?.mutability, Mutability.READING_READONLY)
+                    assert.deepEqual(methodMap.get('ma')?.propertyNames, ['pa', 'pb', 'pc']);
+                    return;
+                }
+            }
         });
     }
 });
