9ac51e6740be9d126a0431e97abf67836343bf34
Grzegorz Pabian <greg@intuita.io>
2022-06-24 18:00:13 +0200

    remove no longer needed utilities

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,37 +1,5 @@
-import {
-    PropertyAccessExpression,
-    ReferencedSymbolEntry,
-    SourceFile,
-    ts,
-} from "ts-morph";
-
 export function isNeitherNullNorUndefined<T>(
     value: NonNullable<T> | null | undefined
 ): value is NonNullable<T> {
     return value !== null && value !== undefined;
-}
-
-// to be removed
-export const calculateStaticPropertyAccessExpressionUpdate = (
-    referencedSymbolEntry: ReferencedSymbolEntry
-): [SourceFile, PropertyAccessExpression] | null => {
-    const sourceFile = referencedSymbolEntry.getSourceFile();
-    const node = referencedSymbolEntry.getNode();
-
-    const propertyAccessExpression = node
-        .getFirstAncestorByKind(
-            ts.SyntaxKind.PropertyAccessExpression
-        );
-
-    if(!propertyAccessExpression) {
-        return null;
-    }
-
-    return [
-        sourceFile,
-        propertyAccessExpression,
-        // () => propertyAccessExpression.replaceWithText(
-        //     name
-        // ),
-    ];
 }
