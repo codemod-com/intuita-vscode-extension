ba088ab95795fbd8ee8f08a189fa7cba0c6f952f
Grzegorz Pabian <greg@intuita.io>
2022-07-05 14:21:26 +0200

    buildCallableMetadataMap

--- src/features/reorderDeclarations/astCommandBuilder.ts
+++ src/features/reorderDeclarations/astCommandBuilder.ts
@@ -1,5 +1,9 @@
 import {NoraNode, ReorderDeclarationFact} from "./factBuilder";
 import {ReorderDeclarationsUserCommand} from "./userCommandBuilder";
+import {getGroupMap} from "../../intuitaExtension/getGroupMap";
+import {Mutability} from "../../intuitaExtension/mutability";
+import {CallableMetadata} from "../../astCommandBuilders/splitClassAstCommandBuilders";
+import {isNeitherNullNorUndefined} from "../../utilities";
 
 export type ReorderDeclarationsAstCommand = Readonly<{
     kind: 'REORDER_DECLARATIONS',
@@ -8,11 +12,55 @@ export type ReorderDeclarationsAstCommand = Readonly<{
     reorderingMap: ReadonlyMap<number, number>,
 }>;
 
+export const buildCallableMetadataMap = (
+    noraNode: NoraNode,
+): ReadonlyMap<string, CallableMetadata> => {
+    if (!('children' in noraNode)) {
+        return new Map();
+    }
+
+    const entries = noraNode
+        .children
+        .map((childNode) => {
+            if (!('node' in childNode)) {
+                return null;
+            }
+
+            const childIdentifiers = Array.from(childNode.childIdentifiers);
+
+            return Array.from(childNode.identifiers)
+                .map(
+                    (identifier) => {
+                        return [
+                            identifier,
+                            {
+                                nonCallableNames: [],
+                                callableNames: childIdentifiers,
+                                mutability: Mutability.READING_READONLY,
+                                empty: false,
+                            }
+                        ] as const;
+                    }
+                );
+        })
+        .filter(isNeitherNullNorUndefined)
+        .flat();
+
+    return new Map(entries);
+};
+
 export const buildReorderDeclarationsAstCommand = (
     userCommand: ReorderDeclarationsUserCommand,
     fact: ReorderDeclarationFact,
 ): ReorderDeclarationsAstCommand => {
-    const { indices } = fact;
+    const { indices, noraNode } = fact;
+
+    const callableMetadataMap = buildCallableMetadataMap(noraNode)
+
+    const groupMap = getGroupMap(
+        callableMetadataMap,
+        null,
+    );
 
     // basic move-by-one algorithm
     const reorderingMap = new Map<number, number>(
