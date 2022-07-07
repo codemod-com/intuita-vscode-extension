bedbb66564b8b9ec50c93171ac1dde52b11cbf66
Grzegorz Pabian <greg@intuita.io>
2022-07-07 14:16:33 +0200

    add calculateAverage to calculateDependencyCoefficient

--- src/features/moveTopLevelNode/3_astCommandBuilder.ts
+++ src/features/moveTopLevelNode/3_astCommandBuilder.ts
@@ -7,13 +7,7 @@ import {MoveTopLevelNodeFact, TopLevelNode} from "./2_factBuilder";
 export const calculateDependencyCoefficient = (
     nodes: ReadonlyArray<TopLevelNode>,
 ): number => {
-    const { length } = nodes;
-
-    if (length === 0) {
-        return 0;
-    }
-
-    const sum = nodes
+    const values = nodes
         .map(
             ({ childIdentifiers }, index) => {
                 return nodes
@@ -30,9 +24,8 @@ export const calculateDependencyCoefficient = (
             }
         )
         .map((value) => Number(value))
-        .reduce((a, b) => a + b, 0);
 
-    return sum / length;
+    return calculateAverage(values);
 };
 
 export const calculateSimilarityCoefficient = (
