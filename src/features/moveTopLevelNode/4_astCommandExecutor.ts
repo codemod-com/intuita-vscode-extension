1ff865c6a99ae1bd5c96339c05a4ed0090914776
Grzegorz Pabian <grzpab@users.noreply.github.com>
2022-07-07 16:44:29 +0200

    ANTLR for C + moving top-level nodes for TS with UX (#22)

---
+++ src/features/moveTopLevelNode/4_astCommandExecutor.ts
@@ -0,0 +1,44 @@
+import {MoveTopLevelNodeAstCommand} from "./3_astCommandBuilder";
+import {moveElementInArray, SourceFileExecution} from "../../utilities";
+
+export const executeMoveTopLevelNodeAstCommand = (
+    {
+        fileName,
+        oldIndex,
+        newIndex,
+        stringNodes,
+    }: MoveTopLevelNodeAstCommand
+): ReadonlyArray<SourceFileExecution> => {
+    if (oldIndex === newIndex) {
+        return [];
+    }
+
+    const topLevelNodeTexts = stringNodes
+        .filter((stringNode) => stringNode.topLevelNodeIndex !== null)
+        .map(({ text }) => text);
+
+    const movedTopLevelNodeTexts = moveElementInArray(
+        topLevelNodeTexts,
+        oldIndex,
+        newIndex,
+    );
+
+    const text = stringNodes
+        .map(
+            ({ topLevelNodeIndex, text}) => {
+                if (topLevelNodeIndex === null) {
+                    return text;
+                }
+
+                return movedTopLevelNodeTexts[topLevelNodeIndex] ?? '';
+            },
+        )
+        .join('');
+
+    return [
+        {
+            name: fileName,
+            text,
+        }
+    ];
+};
