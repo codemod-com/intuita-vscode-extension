1e2e00320b0e861c266ff31293cd9b8a9bfd7940
Grzegorz Pabian <greg@intuita.io>
2022-07-21 17:13:51 +0200

    part two of removing stale code

--- src/astCommandBuilders/splitClassAstCommandBuilders.ts
+++
@@ -1,8 +0,0 @@
-import { Mutability} from "../intuitaExtension/mutability";
-
-export type CallableMetadata = Readonly<{
-    nonCallableNames: ReadonlyArray<string>,
-    callableNames: ReadonlyArray<string>,
-    mutability: Mutability,
-    empty: boolean,
-}>;
