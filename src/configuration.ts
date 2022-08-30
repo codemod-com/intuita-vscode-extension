a4a4dbdc0e20f833fb240015bab00cb700d535fc
Grzegorz Pabian <greg@intuita.io>
2022-08-30 19:40:55 +0200

    INT-140 integrate Joern

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,24 @@
+import { execSync } from 'node:child_process';
+import { type } from 'node:os';
 import * as vscode from 'vscode';
 import { DEFAULT_TOP_LEVEL_NODE_KIND_ORDER, TopLevelNodeKind } from './features/moveTopLevelNode/2_factBuilders/topLevelNode';
+
+const isJoernAvailable = () => {
+    const operatingSystemName = type();
+
+    if (operatingSystemName !== 'Linux' && operatingSystemName !== 'Darwin') {
+        return false;
+    }
+
+    try {
+        // execSync('which joern-parse joern-vectors');
+        execSync('which joern-parse');
+
+        return true;
+    } catch {
+        return false;
+    }
+};
 
 export const getConfiguration = () => {
     const configuration = vscode.workspace.getConfiguration(
@@ -13,10 +32,13 @@ export const getConfiguration = () => {
 
     const minimumLines = configuration.get<number>('minimumLines') ?? 50;
 
+    const joernAvailable = isJoernAvailable();
+
     return {
         topLevelNodeKindOrder,
         saveDocumentOnJobAccept,
         minimumLines,
+        joernAvailable,
     };
 };
 
--- src/extension.ts
+++ src/extension.ts
@@ -21,6 +21,8 @@ import { buildFileNameHash } from './features/moveTopLevelNode/fileNameHash';
 import { join } from 'node:path';
 import { buildFileUri, buildJobUri } from './fileSystems/uris';
 import { CommandComponent } from './components/commandComponent';
+import { mkdir, mkdirSync, writeFileSync } from 'node:fs';
+import { execSync } from 'node:child_process';
 
 export async function activate(
 	context: vscode.ExtensionContext,
@@ -523,27 +525,121 @@ export async function activate(
 
 	context.subscriptions.push(diagnosticCollection);
 
-	// context.subscriptions.push(
-	// 	vscode.languages.onDidChangeDiagnostics(
-	// 		({ uris }) => {
-	// 			uris
-	// 				.flatMap(
-	// 					(uri) => vscode.languages.getDiagnostics(uri),
-	// 				)
-	// 				.filter(
-	// 					({ source }) => source === 'ts'
-	// 				)
-	// 				.forEach(
-	// 					(diagnostic) => {
-	// 						diagnostic.code;
-	// 						diagnostic.message;
-	// 						diagnostic.range;
-	// 						diagnostic.severity;
-	// 					},
-	// 				);
-	// 		}
-	// 	)
-	// );
+	context.subscriptions.push(
+		vscode.languages.onDidChangeDiagnostics(
+			async ({ uris }) => {
+				const { activeTextEditor } = vscode.window;
+
+				if (!activeTextEditor) {
+					console.error('There is no active text editor despite the changed diagnostics.');
+					
+					return;
+				}
+
+				const configuration = configurationContainer.get();
+				
+				if (!configuration.joernAvailable) {
+					console.error('You either use Windows or you have not installed Joern.');
+					
+					return;
+				}
+
+				const activeUri = activeTextEditor.document.uri.toString();
+
+				if (activeUri.includes('.intuita')) {
+					console.log('The files within the .intuita directory won\'t be inspected.');
+
+					return;
+				}
+
+				uris
+					.filter(
+						(uri) => {
+							return activeUri === uri.toString();
+						},
+					)
+					.map(
+						(uri) => {
+							const diagnostics = vscode
+								.languages
+								.getDiagnostics(uri)
+								.filter(
+									({ source }) => source === 'ts'
+								);
+
+							return {
+								uri,
+								diagnostics,
+							};
+						},
+					)
+					.filter(
+						({ diagnostics }) => {
+							return diagnostics.length === 0;
+						},
+					)
+					.forEach(
+						({ uri }) => {
+							if (uri.scheme !== 'file') {
+								console.error(`Could not evaluate the non-file uri: ${uri.toString()}`);
+							}
+
+							const workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
+
+							if (!workspaceFolder) {
+								console.error(`Could not recognize a workspace folder for the uri: ${uri.toString()}`);
+
+								return;
+							}
+
+							const hash = buildHash(uri.toString());
+
+							const { fsPath } = workspaceFolder.uri;
+
+							const directoryPath = join(
+								fsPath,
+								`/.intuita/${hash}/`,
+							);
+
+							const path = join(
+								fsPath,
+								`/.intuita/${hash}/index.ts`,
+							);
+
+							const cpgFilePath = join(
+								fsPath,
+								`/.intuita/${hash}/cpg.bin`,
+							);
+
+							mkdirSync(
+								directoryPath,
+								{
+									recursive: true,
+								},
+							);
+
+							writeFileSync(
+								path,
+								activeTextEditor.document.getText(),
+								{ encoding: 'utf8', }
+							);
+
+							execSync(
+								'joern-parse --output=$JOERN_PROXY_OUTPUT $JOERN_PROXY_INPUT',
+								{
+									env: {
+										JOERN_PROXY_INPUT: directoryPath,
+										JOERN_PROXY_OUTPUT: cpgFilePath,
+									},
+								},
+							);
+
+							console.log(`Wrote the CPG for ${uri.toString()}`);
+						}
+					);
+			}
+		)
+	);
 }
 
 // this method is called when your extension is deactivated
