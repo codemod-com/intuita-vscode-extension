f3ac7d08432956586555375ace430b968a3c99aa
Grzegorz Pabian <greg@intuita.io>
2022-08-31 23:02:36 +0200

    support calculation of the new top-level node kinds

---
+++ src/features/moveTopLevelNode/2_factBuilders/buildTypeScriptTopLevelNode.ts
@@ -0,0 +1,93 @@
+import { isClassDeclaration, isEnumDeclaration, isFunctionDeclaration, isInterfaceDeclaration, isTypeAliasDeclaration, isVariableDeclaration, isVariableStatement, Node, SyntaxKind } from "typescript";
+import { TopLevelNodeKind } from "./topLevelNode";
+
+const hasExportKeyword = (node: Node): boolean => {
+    return node
+        .modifiers
+        ?.some(
+            (modifier) => modifier.kind === SyntaxKind.ExportKeyword
+        )
+        ?? false;
+};
+
+const hasConstKeyword = (node: Node): boolean => {
+    return node
+        .modifiers
+        ?.some(
+            (modifier) => modifier.kind === SyntaxKind.ConstKeyword
+        )
+        ?? false;
+};
+
+export const getTopLevelNodeKind = (
+    node: Node,
+): TopLevelNodeKind => {
+    if (isClassDeclaration(node)) {
+        return hasExportKeyword(node)
+            ? TopLevelNodeKind.exportClass
+            : TopLevelNodeKind.class;
+    }
+
+    if (isTypeAliasDeclaration(node)) {
+        return hasExportKeyword(node)
+            ? TopLevelNodeKind.exportType
+            : TopLevelNodeKind.type;
+    }
+
+    if (isInterfaceDeclaration(node)) {
+        return hasExportKeyword(node)
+            ? TopLevelNodeKind.exportInterface
+            : TopLevelNodeKind.interface;
+    }
+
+    if (isEnumDeclaration(node)) {
+        return hasExportKeyword(node)
+            ? TopLevelNodeKind.exportEnum
+            : TopLevelNodeKind.enum;   
+    }
+
+    if (isFunctionDeclaration(node)) {
+        return hasExportKeyword(node)
+            ? TopLevelNodeKind.exportFunction
+            : TopLevelNodeKind.function;
+    }
+
+    if (isVariableStatement(node)) {
+        const { declarationList } = node;
+        const { declarations } = declarationList;
+
+        const doesHaveExportKeyword = hasExportKeyword(node);
+
+        if (declarations.length !== 1) {
+            return doesHaveExportKeyword
+                ? TopLevelNodeKind.exportManyVariables
+                : TopLevelNodeKind.manyVariables;
+        }
+
+        const doesHaveConstKeyword = hasConstKeyword(declarationList);
+
+        const arrowFunction = declarations[0]?.initializer?.kind === SyntaxKind.ArrowFunction;
+
+        if (arrowFunction) {
+            if (doesHaveConstKeyword) {
+                return doesHaveExportKeyword
+                    ? TopLevelNodeKind.exportConstArrowFunction
+                    : TopLevelNodeKind.constArrowFunction;
+            }
+
+            return doesHaveExportKeyword
+                ? TopLevelNodeKind.exportLetArrowFunction
+                : TopLevelNodeKind.letArrowFunction;
+        }
+
+        if (doesHaveConstKeyword) {
+            return doesHaveExportKeyword
+                ? TopLevelNodeKind.exportConstVariable
+                : TopLevelNodeKind.constVariable;
+        }
+
+        return doesHaveExportKeyword
+            ? TopLevelNodeKind.exportLetVariable
+            : TopLevelNodeKind.letVariable;
+    }
+}
