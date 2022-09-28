b948eea481cd05a37eef710634bc55a7e8a13c68
Grzegorz Pabian <greg@intuita.io>
2022-09-28 17:23:41 +0200

    INT-191 ensure that types/kinds returned from that function are within defined boundaries

---
+++ src/features/repairCode/extractKindsFromTs2345ErrorMessage.ts
@@ -0,0 +1,23 @@
+export const extractKindsFromTs2345ErrorMessage = (
+    message: string
+) => {
+    const kinds = message
+        .replace('Argument of type \'', '')
+        .replace('\' is not assignable to parameter of type \'', '|')
+        .replace('\'.', '')
+        .split('|')
+        .slice(0, 2)
+        .filter(
+            (kind): kind is ('boolean' | 'number' | 'string') =>
+                kind === 'boolean' || kind === 'number' || kind === 'string'
+        );
+
+    if (kinds.length !== 2) {
+        return null;
+    }
+
+    return {
+        expected: kinds[1],
+        received: kinds[0],
+    };
+};
--- src/test/extractTypesFromTs2345ErrorMessage.test.ts
+++ src/test/extractTypesFromTs2345ErrorMessage.test.ts
@@ -1,22 +1,27 @@
-import {extractTypesFromTs2345ErrorMessage} from "../features/repairCode/extractTypesFromTs2345ErrorMessage";
+import {extractKindsFromTs2345ErrorMessage} from "../features/repairCode/extractKindsFromTs2345ErrorMessage";
 import {assert} from "chai";
+import {assertsNeitherNullOrUndefined} from "../utilities";
 
 describe('extractTypesFromTs2345ErrorMessage', () => {
     it('should return string and number', () => {
-        const types = extractTypesFromTs2345ErrorMessage(
+        const kinds = extractKindsFromTs2345ErrorMessage(
             'Argument of type \'string\' is not assignable to parameter of type \'number\'.',
         );
 
-        assert.equal(types.received, 'string');
-        assert.equal(types.expected, 'number');
+        assertsNeitherNullOrUndefined(kinds);
+
+        assert.equal(kinds.received, 'string');
+        assert.equal(kinds.expected, 'number');
     });
 
     it('should return boolean and string', () => {
-        const types = extractTypesFromTs2345ErrorMessage(
+        const kinds = extractKindsFromTs2345ErrorMessage(
             'Argument of type \'boolean\' is not assignable to parameter of type \'string\'.',
         );
 
-        assert.equal(types.received, 'boolean');
-        assert.equal(types.expected, 'string');
+        assertsNeitherNullOrUndefined(kinds);
+
+        assert.equal(kinds.received, 'boolean');
+        assert.equal(kinds.expected, 'string');
     });
 });
