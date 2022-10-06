bd0b8a60efb7216490e42dbf4f6dde5844b08630
Grzegorz Pabian <grzpab@users.noreply.github.com>
2022-10-06 17:42:18 +0200

    INT-197 Rule-based Code Repair: Remove quotes instead of wrapping (#90)

--- src/features/repairCode/buildReplacement.ts
+++ src/features/repairCode/buildReplacement.ts
@@ -1,16 +1,82 @@
-export const buildReplacement = (
-	text: string,
-	expectedKind: 'boolean' | 'number' | 'string',
-): string => {
+type ReplacementArguments = Readonly<{
+	text: string;
+	receivedKind: 'boolean' | 'number' | 'string';
+	expectedKind: 'boolean' | 'number' | 'string';
+}>;
+
+const regexp = /^(-|\+)?[0-9]+(.[0-9]+)?$/;
+
+export const buildReplacement = ({
+	expectedKind,
+	receivedKind,
+	text,
+}: ReplacementArguments): string => {
+	const surroundedBySingleQuote = text.startsWith("'") && text.endsWith("'");
+	const surroundedByDoubleQuote = text.startsWith('"') && text.endsWith('"');
+
 	if (expectedKind === 'boolean') {
+		if (receivedKind === 'string') {
+			if (surroundedBySingleQuote || surroundedByDoubleQuote) {
+				const value = text.slice(1, text.length - 1);
+
+				if (value === 'false' || value === 'true') {
+					return value;
+				}
+			}
+		}
+
+		if (receivedKind === 'number') {
+			if (text === '0') {
+				return 'false';
+			}
+
+			if (regexp.test(text)) {
+				return 'true';
+			}
+		}
+
 		return `Boolean(${text})`;
 	}
 
 	if (expectedKind === 'number') {
+		if (receivedKind === 'string') {
+			if (surroundedBySingleQuote || surroundedByDoubleQuote) {
+				const value = text.slice(1, text.length - 1);
+
+				if (value.length === 0) {
+					return '0';
+				}
+
+				if (regexp.test(value)) {
+					return value;
+				}
+			}
+		}
+
+		if (receivedKind === 'boolean') {
+			if (text === 'false') {
+				return '0';
+			}
+
+			if (text === 'true') {
+				return '1';
+			}
+		}
+
 		return `Number(${text})`;
 	}
 
 	if (expectedKind === 'string') {
+		if (receivedKind === 'boolean') {
+			if (text === 'false' || text === 'true') {
+				return `'${text}'`;
+			}
+		}
+
+		if (regexp.test(text)) {
+			return `'${text}'`;
+		}
+
 		return `String(${text})`;
 	}
 
---
+++ src/test/buildReplacement.test.ts
@@ -0,0 +1,234 @@
+import { assert } from 'chai';
+import { buildReplacement } from '../features/repairCode/buildReplacement';
+
+describe('buildReplacement', () => {
+	it("should change the '' string into the 0 number", () => {
+		const replacement = buildReplacement({
+			text: "''",
+			receivedKind: 'string',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '0');
+	});
+
+	it("should change the '-123456789.12345678' string into the number", () => {
+		const replacement = buildReplacement({
+			text: "'-123456789.12345678'",
+			receivedKind: 'string',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '-123456789.12345678');
+	});
+
+	it("should change the '20' string into the 20 number", () => {
+		const replacement = buildReplacement({
+			text: "'20'",
+			receivedKind: 'string',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '20');
+	});
+
+	it('should change the "20" string into the 20 number', () => {
+		const replacement = buildReplacement({
+			text: '"20"',
+			receivedKind: 'string',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '20');
+	});
+
+	it('should change the stringVariable string into the wrapped number', () => {
+		const replacement = buildReplacement({
+			text: 'stringVariable',
+			receivedKind: 'string',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, 'Number(stringVariable)');
+	});
+
+	it("should change the 0 number into '0' string", () => {
+		const replacement = buildReplacement({
+			text: '0',
+			receivedKind: 'number',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, "'0'");
+	});
+
+	it("should change the -123456789.123456789 number into '-123456789.123456789' string", () => {
+		const replacement = buildReplacement({
+			text: '-123456789.123456789',
+			receivedKind: 'number',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, "'-123456789.123456789'");
+	});
+
+	it("should change the 20 number into the '20' string", () => {
+		const replacement = buildReplacement({
+			text: '20',
+			receivedKind: 'number',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, "'20'");
+	});
+
+	it('should change the numberVariable number into the wrapper string', () => {
+		const replacement = buildReplacement({
+			text: 'numberVariable',
+			receivedKind: 'number',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, 'String(numberVariable)');
+	});
+
+	it("should change the 'false' string into the false boolean", () => {
+		const replacement = buildReplacement({
+			text: "'false'",
+			receivedKind: 'string',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'false');
+	});
+
+	it("should change the 'true' string into the true boolean", () => {
+		const replacement = buildReplacement({
+			text: "'true'",
+			receivedKind: 'string',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'true');
+	});
+
+	it('should change the "false" string into the false boolean', () => {
+		const replacement = buildReplacement({
+			text: '"false"',
+			receivedKind: 'string',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'false');
+	});
+
+	it('should change the "true" string into the false boolean', () => {
+		const replacement = buildReplacement({
+			text: '"true"',
+			receivedKind: 'string',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'true');
+	});
+
+	it('should change the stringVariable string into the wrapper boolean', () => {
+		const replacement = buildReplacement({
+			text: 'stringVariable',
+			receivedKind: 'string',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'Boolean(stringVariable)');
+	});
+
+	it('should change the false boolean into the "false" string', () => {
+		const replacement = buildReplacement({
+			text: 'false',
+			receivedKind: 'boolean',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, "'false'");
+	});
+
+	it('should change the true boolean into the "true" string', () => {
+		const replacement = buildReplacement({
+			text: 'true',
+			receivedKind: 'boolean',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, "'true'");
+	});
+
+	it('should change the booleanVariable boolean into the wrapped string', () => {
+		const replacement = buildReplacement({
+			text: 'booleanVariable',
+			receivedKind: 'boolean',
+			expectedKind: 'string',
+		});
+
+		assert.equal(replacement, 'String(booleanVariable)');
+	});
+
+	it('should change the 0 number into the false boolean', () => {
+		const replacement = buildReplacement({
+			text: '0',
+			receivedKind: 'number',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'false');
+	});
+
+	it('should change the -123456789.123456789 number into the true boolean', () => {
+		const replacement = buildReplacement({
+			text: '-123456789.123456789',
+			receivedKind: 'number',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'true');
+	});
+
+	it('should change the numberVariable number into the wrapper boolean', () => {
+		const replacement = buildReplacement({
+			text: 'numberVariable',
+			receivedKind: 'number',
+			expectedKind: 'boolean',
+		});
+
+		assert.equal(replacement, 'Boolean(numberVariable)');
+	});
+
+	it('should change the false boolean into the 0 number', () => {
+		const replacement = buildReplacement({
+			text: 'false',
+			receivedKind: 'boolean',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '0');
+	});
+
+	it('should change the true boolean into the 1 number', () => {
+		const replacement = buildReplacement({
+			text: 'true',
+			receivedKind: 'boolean',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '1');
+	});
+
+	it('should change the booleanVariable boolean into the wrapper number', () => {
+		const replacement = buildReplacement({
+			text: 'true',
+			receivedKind: 'boolean',
+			expectedKind: 'number',
+		});
+
+		assert.equal(replacement, '1');
+	});
+});
