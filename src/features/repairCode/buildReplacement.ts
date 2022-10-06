096d389db73dd7dee8a8138d4aebc1bca349cf54
Greg Pabian <greg@intuita.io>
2022-10-06 10:16:33 +0200

    implement string->number transition

--- src/features/repairCode/buildReplacement.ts
+++ src/features/repairCode/buildReplacement.ts
@@ -4,20 +4,43 @@ type ReplacementArguments = Readonly<{
 	expectedKind: 'boolean' | 'number' | 'string',
 }>;
 
+const regexp = /^(-|\+)?[0-9]+(.[0-9]+)?$/;
+
 export const buildReplacement = (
-	replacementArguments: ReplacementArguments,
+	{
+		expectedKind,
+		receivedKind,
+		text,
+	}: ReplacementArguments,
 ): string => {
-	if (replacementArguments.expectedKind === 'boolean') {
-		return `Boolean(${replacementArguments.text})`;
+	if (expectedKind === 'boolean') {
+		return `Boolean(${text})`;
 	}
 
-	if (replacementArguments.expectedKind === 'number') {
-		return `Number(${replacementArguments.text})`;
+	if (expectedKind === 'number') {
+		if (receivedKind === 'string') {
+			const surroundedBySingleQuote = text.startsWith('\'') && text.endsWith('\'');
+			const surroundedByDoubleQuote = text.startsWith('"') && text.endsWith('"');
+
+			if (surroundedBySingleQuote || surroundedByDoubleQuote) {
+				const value = text.slice(1, text.length - 1);
+
+				if (value.length === 0) {
+					return '0';
+				}
+
+				if (regexp.test(value)) {
+					return value;
+				}
+			}
+		}
+
+		return `Number(${text})`;
 	}
 
-	if (replacementArguments.expectedKind === 'string') {
-		return `String(${replacementArguments.text})`;
+	if (expectedKind === 'string') {
+		return `String(${text})`;
 	}
 
-	return replacementArguments.text;
+	return text;
 };
--- src/test/buildReplacement.test.ts
+++ src/test/buildReplacement.test.ts
@@ -1,7 +1,27 @@
 import { assert } from "chai";
 import { buildReplacement } from "../features/repairCode/buildReplacement";
 
-describe('buildReplacement', () => {
+describe.only('buildReplacement', () => {
+    it('should change the \'\' string into the 0 number', () => {
+        const replacement = buildReplacement({
+            text: '\'\'',
+            receivedKind: 'string',
+            expectedKind: 'number',
+        });
+
+        assert.equal(replacement, '0');
+    });
+
+    it('should change the \'-123456789.12345678\' string into the number', () => {
+        const replacement = buildReplacement({
+            text: '\'-123456789.12345678\'',
+            receivedKind: 'string',
+            expectedKind: 'number',
+        });
+
+        assert.equal(replacement, '-123456789.12345678');
+    });
+
     it('should change the \'20\' string into the 20 number', () => {
         const replacement = buildReplacement({
             text: '\'20\'',
