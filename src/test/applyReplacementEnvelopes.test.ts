e7f97ff41f1e067f21c1ff04cbb123de7025256a
Greg Pabian <greg@intuita.io>
2022-10-31 12:21:32 +0100

    fix the algorithm

--- src/test/applyReplacementEnvelopes.test.ts
+++ src/test/applyReplacementEnvelopes.test.ts
@@ -5,15 +5,11 @@ const applyReplacementEnvelopes = (
     text: string,
     replacementEnvelopes: ReadonlyArray<ReplacementEnvelope>,
 ): string => {
-    if (replacementEnvelopes.length === 0) {
-        return text;
-    }
-
-    let newText: string = '';
+    let newText: string = text;
     let shift: number = 0;
 
     for (const { range, replacement } of replacementEnvelopes) {
-        newText = text.slice(0, range.start + shift) + replacement + text.slice(range.end + shift);
+        newText = newText.slice(0, range.start + shift) + replacement + newText.slice(range.end + shift);
 
         shift += replacement.length - (range.end - range.start);
     }
@@ -24,14 +20,14 @@ const applyReplacementEnvelopes = (
 describe.only('applyReplacementEnvelopes', () => {
     const oldText = '01234567890123456789012345678901234567890123456789';
 
-    it('should do nothing for no replacement envelopes', () => {
+    it('should do 0 replacements for no replacement envelopes', () => {
         const newText = applyReplacementEnvelopes(oldText, []);
 
         assert.equal(newText, oldText);
         assert.equal(newText.length, 50);
     })
 
-    it('should do one replacement with keeping the original length', () => {
+    it('should do 1 replacement with keeping the original length', () => {
         const replacement: ReplacementEnvelope = {
             range: {
                 start: 10,
@@ -46,7 +42,7 @@ describe.only('applyReplacementEnvelopes', () => {
         assert.equal(newText.length, 50);
     })
 
-    it('should do one replacement with shrinking the original length', () => {
+    it('should do 1 replacement with shrinking the original length', () => {
         const replacement: ReplacementEnvelope = {
             range: {
                 start: 10,
@@ -61,7 +57,7 @@ describe.only('applyReplacementEnvelopes', () => {
         assert.equal(newText.length, 45);
     });
 
-    it('should do one replacement with expanding the original length', () => {
+    it('should do 1 replacement with expanding the original length', () => {
         const replacement: ReplacementEnvelope = {
             range: {
                 start: 10,
@@ -75,4 +71,35 @@ describe.only('applyReplacementEnvelopes', () => {
         assert.equal(newText, '0123456789abcdefghijklmno012345678901234567890123456789');
         assert.equal(newText.length, 55);
     })
+
+    it('should do empty 3 replacements with shrinking the original length', () => {
+        const replacements: ReplacementEnvelope[] = [
+            {
+                range: {
+                    start: 5,
+                    end: 10,
+                },
+                replacement: ''
+            },
+            {
+                range: {
+                    start: 20,
+                    end: 30,
+                },
+                replacement: ''
+            },
+            {
+                range: {
+                    start: 40,
+                    end: 45,
+                },
+                replacement: ''
+            }
+        ]
+
+        const newText = applyReplacementEnvelopes(oldText, replacements);
+
+        assert.equal(newText, '012340123456789012345678956789');
+        assert.equal(newText.length, 30);
+    });
 });
