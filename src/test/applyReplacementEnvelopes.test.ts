1726415111a69a333df058b37d332aac35ef98f4
Greg Pabian <greg@intuita.io>
2022-10-31 12:28:09 +0100

    fix

--- src/test/applyReplacementEnvelopes.test.ts
+++ src/test/applyReplacementEnvelopes.test.ts
@@ -27,7 +27,7 @@ describe.only('applyReplacementEnvelopes', () => {
         assert.equal(newText.length, 50);
     })
 
-    it('should do 1 replacement with keeping the original length', () => {
+    it('should do 1 replacement with maintaining the original length', () => {
         const replacement: ReplacementEnvelope = {
             range: {
                 start: 10,
@@ -101,5 +101,67 @@ describe.only('applyReplacementEnvelopes', () => {
 
         assert.equal(newText, '012340123456789012345678956789');
         assert.equal(newText.length, 30);
+    });
+
+    it('should do empty 3 replacements with expanding the original length', () => {
+        const replacements: ReplacementEnvelope[] = [
+            {
+                range: {
+                    start: 5,
+                    end: 10,
+                },
+                replacement: 'aaaaaaaaaaaaaaa' // +10
+            },
+            {
+                range: {
+                    start: 20,
+                    end: 30,
+                },
+                replacement: 'bbbbbbbbbbbbbbb' // +5
+            },
+            {
+                range: {
+                    start: 40,
+                    end: 45,
+                },
+                replacement: 'ccccccccccccccc' // +10
+            }
+        ]
+
+        const newText = applyReplacementEnvelopes(oldText, replacements);
+
+        assert.equal(newText, '01234aaaaaaaaaaaaaaa0123456789bbbbbbbbbbbbbbb0123456789ccccccccccccccc56789');
+        assert.equal(newText.length, 50 + 25);
+    });
+
+    it('should do empty 3 replacements with different replacements', () => {
+        const replacements: ReplacementEnvelope[] = [
+            {
+                range: {
+                    start: 5,
+                    end: 10,
+                },
+                replacement: '' // -5
+            },
+            {
+                range: {
+                    start: 20,
+                    end: 30,
+                },
+                replacement: 'bbbbbbbbbbbbbbb' // +5
+            },
+            {
+                range: {
+                    start: 40,
+                    end: 45,
+                },
+                replacement: '' // -5
+            }
+        ]
+
+        const newText = applyReplacementEnvelopes(oldText, replacements);
+
+        assert.equal(newText, '012340123456789bbbbbbbbbbbbbbb012345678956789');
+        assert.equal(newText.length, 50 - 5);
     });
 });
