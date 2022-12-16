73efaff00a470cfe3b0ce536e81ca9de091dee8b
Grzegorz Pabian <grzpab@users.noreply.github.com>
2022-12-16 11:22:12 +0100

    wip

--- src/persistedState/persistedStateService.ts
+++ src/persistedState/persistedStateService.ts
@@ -1,3 +1,4 @@
+import { FileSystem, Uri, WorkspaceFolder } from "vscode";
 import { CaseManager } from "../cases/caseManager";
 import { JobManager } from "../components/jobManager";
 import { MessageBus, MessageKind } from "../components/messageBus";
@@ -7,6 +8,8 @@ import { mapCaseToPersistedCase, mapJobToPersistedJob } from "./mappers";
 export class PersistedStateService {
     constructor(
         private readonly caseManager: CaseManager,
+        private readonly fileSystem: FileSystem,
+        private readonly getWorkspaceFolders: () => ReadonlyArray<WorkspaceFolder>,
         private readonly jobManager: JobManager,
         private readonly messageBus: MessageBus,
     ) {
@@ -23,22 +26,39 @@ export class PersistedStateService {
         )
     }
 
-    #onPersistStateMessage () {
+    async #onPersistStateMessage () {
+        const workspaceFolders = this.getWorkspaceFolders();
+
+        const uri = workspaceFolders[0]?.uri;
+
+        if (!uri) {
+            console.error("No workspace folder found. We cannot persist the state anywhere.");
+
+            return;
+        }
+
+        const persistedState = this.#buildPersistedState();
+        const buffer = Buffer.from(JSON.stringify(persistedState));
+
+        const intuitaDirectoryUri = Uri.joinPath(uri, ".intuita");
+        await this.fileSystem.createDirectory(intuitaDirectoryUri);
+
+        const localStateUri = Uri.joinPath(intuitaDirectoryUri, "localState.json");
+        this.fileSystem.writeFile(localStateUri, buffer);
+    }
+
+    #buildPersistedState(): PersistedState {
         const cases = Array.from(this.caseManager.getCases()).map(kase => mapCaseToPersistedCase(kase));
         const caseHashJobHashes = Array.from(this.caseManager.getCaseHashJobHashSetValues());
 
         const jobs = Array.from(this.jobManager.getJobs()).map(job => mapJobToPersistedJob(job));
-
         const rejectedJobHashes = Array.from(this.jobManager.getRejectedJobHashes());
 
-        const persistedState: PersistedState = {
+        return {
             cases,
             caseHashJobHashes,
             jobs,
             rejectedJobHashes,
         }
-
-        
-
     }
 }
