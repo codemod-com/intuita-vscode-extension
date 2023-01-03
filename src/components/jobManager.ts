1f0845f6080779838dce0974cfcc3d4931e9c711
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-01-03 13:58:41 +0100

    wip

--- src/components/jobManager.ts
+++ src/components/jobManager.ts
@@ -132,68 +132,10 @@ export class JobManager {
 	async #onAcceptJobsMessage(
 		message: Message & { kind: MessageKind.acceptJobs },
 	) {
-		const uriJobOutputs: [Uri, Uri][] = [];
-		const deletedJobHashes = new Set<JobHash>();
-
-		for (const { uriHash, jobHashes } of this.#getUriHashesWithJobHashes(
-			message.jobHashes,
-		)) {
-			const jobs = Array.from(jobHashes)
-				.map((jobHash) => this.#jobMap.get(jobHash))
-				.filter(isNeitherNullNorUndefined);
-
-			if (jobs[0]) {
-				const uri = jobs[0].inputUri;
-
-				uriJobOutputs.push([uri, jobs[0].outputUri]);
-			}
-
-			const otherJobHashes =
-				this.#uriHashJobHashSetManager.getRightHashesByLeftHash(
-					uriHash,
-				);
-
-			for (const jobHash of otherJobHashes) {
-				this.#uriHashJobHashSetManager.delete(uriHash, jobHash);
-				this.#jobMap.delete(jobHash);
-			}
-		}
-
-		uriJobOutputs.forEach(([uri, jobOutputUri]) => {
-			this.#messageBus.publish({
-				kind: MessageKind.updateExternalFile,
-				uri,
-				contentUri: jobOutputUri,
-			});
-		});
-
-		this.#messageBus.publish({
-			kind: MessageKind.jobsAccepted,
-			deletedJobHashes,
-		});
-	}
-
-	#onRejectJobsMessage(message: Message & { kind: MessageKind.rejectJobs }) {
-		const codemodHashJobHashSetManager = new LeftRightHashSetManager<CodemodHash, JobHash>(new Set());
-		const codemods = new Map<CodemodHash, Codemod>();
-
-		for (const jobHash of message.jobHashes) {
-			const job = this.#jobMap.get(jobHash);
-
-            if (!job) {
-				continue;
-            }
-
-			const codemod: Codemod = {
-				setName: job.codemodSetName,
-				name: job.codemodName,
-			};
-
-			const codemodHash = buildCodemodHash(codemod);
-
-			codemodHashJobHashSetManager.upsert(codemodHash, jobHash);
-			codemods.set(codemodHash, codemod);
-		}
+		const {
+			codemodHashJobHashSetManager,
+			codemods,
+		} = this.#buildCodemodObjects(message.jobHashes);
 
 		const messages: Message[] = [];
 
@@ -202,7 +144,75 @@ export class JobManager {
 		
 			for (const codemodHash of codemodHashes) {
 				const deletedJobHashes = codemodHashJobHashSetManager.getRightHashesByLeftHash(codemodHash);
+				const codemod = codemods.get(codemodHash);
 
+				if (!deletedJobHashes || !codemod ) {
+                    continue;
+                }
+
+				messages.push({
+					kind: MessageKind.jobsAccepted,
+					deletedJobHashes,
+					codemodSetName: codemod.setName,
+					codemodName: codemod.name,
+				});
+			}
+		}
+
+		{
+			const uriJobOutputs: [Uri, Uri][] = [];
+
+			for (const { uriHash, jobHashes } of this.#getUriHashesWithJobHashes(
+				message.jobHashes,
+			)) {
+				const jobs = Array.from(jobHashes)
+					.map((jobHash) => this.#jobMap.get(jobHash))
+					.filter(isNeitherNullNorUndefined);
+
+				if (jobs[0]) {
+					const uri = jobs[0].inputUri;
+
+					uriJobOutputs.push([uri, jobs[0].outputUri]);
+				}
+
+				const otherJobHashes =
+					this.#uriHashJobHashSetManager.getRightHashesByLeftHash(
+						uriHash,
+					);
+
+				for (const jobHash of otherJobHashes) {
+					this.#uriHashJobHashSetManager.delete(uriHash, jobHash);
+					this.#jobMap.delete(jobHash);
+				}
+			}
+
+			uriJobOutputs.forEach(([uri, jobOutputUri]) => {
+				messages.push({
+					kind: MessageKind.updateExternalFile,
+					uri,
+					contentUri: jobOutputUri,
+				});
+			});
+		}
+
+		for (const message of messages) {
+			this.#messageBus.publish(message);
+		}
+	}
+
+	#onRejectJobsMessage(message: Message & { kind: MessageKind.rejectJobs }) {
+		const {
+			codemodHashJobHashSetManager,
+			codemods,
+		} = this.#buildCodemodObjects(message.jobHashes);
+
+		const messages: Message[] = [];
+
+		{
+			const codemodHashes = codemodHashJobHashSetManager.getLeftHashes();
+		
+			for (const codemodHash of codemodHashes) {
+				const deletedJobHashes = codemodHashJobHashSetManager.getRightHashesByLeftHash(codemodHash);
 				const codemod = codemods.get(codemodHash);
 
 				if (!deletedJobHashes || !codemod ) {
@@ -227,6 +237,34 @@ export class JobManager {
 		for (const message of messages) {
 			this.#messageBus.publish(message);
 		}
+	}
+
+	#buildCodemodObjects(jobHashes: ReadonlySet<JobHash>) {
+		const codemodHashJobHashSetManager = new LeftRightHashSetManager<CodemodHash, JobHash>(new Set());
+		const codemods = new Map<CodemodHash, Codemod>();
+
+		for (const jobHash of jobHashes) {
+			const job = this.#jobMap.get(jobHash);
+
+            if (!job) {
+				continue;
+            }
+
+			const codemod: Codemod = {
+				setName: job.codemodSetName,
+				name: job.codemodName,
+			};
+
+			const codemodHash = buildCodemodHash(codemod);
+
+			codemodHashJobHashSetManager.upsert(codemodHash, jobHash);
+			codemods.set(codemodHash, codemod);
+		}
+
+		return {
+			codemodHashJobHashSetManager,
+			codemods,
+		};
 	}
 
 	#onClearStateMessage() {
