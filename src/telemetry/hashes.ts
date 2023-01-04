28ebf9f1f3345409304c50f3d47c7bcb4102b2fa
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-01-04 15:38:00 +0100

    INT-335 use readBigInt64BE and readInt16BE

--- src/telemetry/hashes.ts
+++ src/telemetry/hashes.ts
@@ -3,14 +3,14 @@ import { randomBytes } from 'node:crypto';
 export const buildSessionId = (): string => {
 	const buffer = randomBytes(8);
 
-	const bigUint = buffer.readBigUint64BE();
+	const bigUint = buffer.readBigInt64BE();
 
 	return String(bigUint);
 };
 
 export const buildExecutionId = (): string => {
 	const buffer = randomBytes(2);
-	const uint = buffer.readUint16BE();
+	const uint = buffer.readInt16BE();
 
 	return String(uint);
 };
