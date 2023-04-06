97fb38673593c70a09ce357f47f71f18d8dc25a0
Benny Joo <sldisek783@gmail.com>
2023-04-06 12:55:48 +0100

    [IDE ext] Create Navigation for Executing Recipe on Path (#219)

--- src/recipes/codecs.ts
+++ src/recipes/codecs.ts
@@ -15,4 +15,46 @@ export const recipeNameCodec = t.union([
 
 export type RecipeName = t.TypeOf<typeof recipeNameCodec>;
 
-export const RECIPE_NAMES = recipeNameCodec.types.map((type) => type.value);
+export const projectNameCodec = t.union([
+	t.literal('Next.js'),
+	t.literal('MUI'),
+	t.literal('React Redux'),
+	t.literal('React Router'),
+	t.literal('Immutable.js'),
+	t.literal('RedwoodJS'),
+]);
+export type ProjectName = t.TypeOf<typeof projectNameCodec>;
+export const PROJECT_NAMES = projectNameCodec.types.map((type) => type.value);
+
+export const RECIPE_MAP: Map<ProjectName, Record<string, RecipeName>> = new Map(
+	PROJECT_NAMES.map((projectName: ProjectName) => {
+		let versionMap: Record<string, RecipeName>;
+		switch (projectName) {
+			case 'Next.js':
+				versionMap = {
+					'13': 'nextJs',
+					Composite: 'next_13_composite',
+				};
+				break;
+			case 'MUI':
+				versionMap = { '5': 'mui' };
+				break;
+			case 'React Redux':
+				versionMap = { '0': 'react-redux_0' };
+				break;
+			case 'React Router':
+				versionMap = { '4': 'reactrouterv4', '6': 'reactrouterv6' };
+				break;
+			case 'Immutable.js':
+				versionMap = { '0': 'immutablejsv0', '4': 'immutablejsv4' };
+				break;
+			case 'RedwoodJS':
+				versionMap = {
+					'4': 'redwoodjs_core_4',
+					Experimental: 'redwoodjs_experimental',
+				};
+				break;
+		}
+		return [projectName, versionMap];
+	}),
+);
