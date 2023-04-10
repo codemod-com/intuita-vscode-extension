fe629369923c9baee05e6df4b26ab34d55888253
Henok <henok.tsegaye@captario.com>
2023-04-10 15:52:39 +0300

    remove un-used files

--- src/fileWatcher.ts
+++ src/fileWatcher.ts
@@ -1,4 +1,4 @@
-import { workspace, Disposable, Uri } from 'vscode';
+import { workspace, Disposable } from 'vscode';
 
 export const watchFileWithPattern = (
 	path: string,
@@ -24,42 +24,6 @@ export const watchFileWithPattern = (
 	return {
 		dispose: () => {
 			watcher.dispose();
-		},
-	};
-};
-
-export const watchFiles = (
-	filePath: Uri[],
-	callback: () => void,
-): Disposable => {
-	const watchers = filePath.map((el) =>
-		workspace.createFileSystemWatcher(el.fsPath.toString()),
-	);
-
-	// watch for changes
-	watchers.forEach((watcher) =>
-		watcher.onDidChange(() => {
-			callback();
-		}),
-	);
-
-	// watch for creation
-	watchers.forEach((watcher) =>
-		watcher.onDidCreate(() => {
-			callback();
-		}),
-	);
-
-	// watch for deletion
-	watchers.forEach((watcher) =>
-		watcher.onDidDelete(() => {
-			callback();
-		}),
-	);
-
-	return {
-		dispose: () => {
-			watchers.forEach((el) => el.dispose());
 		},
 	};
 };
