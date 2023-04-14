93acb6324ea48f0b7775de6f8a79ae860402c468
Benny Joo <sldisek783@gmail.com>
2023-04-14 14:49:34 +0100

    fix build again

--- intuita-webview/src/jobDiffView/index.tsx
+++ intuita-webview/src/jobDiffView/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { loader } from '@monaco-editor/react';
 import '../shared/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
-
-loader.init();
 
 root.render(
 	<React.StrictMode>
