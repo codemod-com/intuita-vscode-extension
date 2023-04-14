2628f95a2bf47aa9faa5bcf55404c45feb34f0a9
Benny Joo <sldisek783@gmail.com>
2023-04-14 14:48:32 +0100

    fix build

--- intuita-webview/src/jobDiffView/index.tsx
+++ intuita-webview/src/jobDiffView/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as monaco from 'monaco-editor';
 import { loader } from '@monaco-editor/react';
 import '../shared/index.css';
 import App from './App';
@@ -9,7 +8,6 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 
-loader.config({ monaco });
 loader.init();
 
 root.render(
