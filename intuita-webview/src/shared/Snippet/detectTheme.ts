ee698b2c71a1e9170e8dc86f1e56b3f0fbc05aa9
Henok <henok.tsegaye@toptal.com>
2023-04-19 17:58:11 +0300

    update: update code on detectBaseTheme

--- intuita-webview/src/shared/Snippet/detectTheme.ts
+++ intuita-webview/src/shared/Snippet/detectTheme.ts
@@ -1,14 +1,10 @@
 export function detectBaseTheme(): 'vs-light' | 'vs-dark' {
-	const body = document.body;
-	if (body) {
-		switch (body.getAttribute('data-vscode-theme-kind')) {
-			default:
-			case 'vscode-light':
-				return 'vs-light';
-			case 'vscode-dark':
-			case 'vscode-high-contrast':
-				return 'vs-dark';
-		}
+	const attribute = document.body.getAttribute('data-vscode-theme-kind');
+	if (
+		attribute &&
+		(attribute === 'vscode-dark' || attribute === 'vscode-high-contrast')
+	) {
+		return 'vs-dark';
 	}
 
 	return 'vs-light';
