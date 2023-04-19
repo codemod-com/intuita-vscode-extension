811601c2091f4d996e9a9fe6545822efc5f475b7
Henok <henok.tsegaye@toptal.com>
2023-04-19 18:07:02 +0300

    update: update attribute

--- intuita-webview/src/shared/Snippet/detectTheme.ts
+++ intuita-webview/src/shared/Snippet/detectTheme.ts
@@ -1,9 +1,6 @@
 export function detectBaseTheme(): 'vs-light' | 'vs-dark' {
 	const attribute = document.body.getAttribute('data-vscode-theme-kind');
-	if (
-		attribute &&
-		(attribute === 'vscode-dark' || attribute === 'vscode-high-contrast')
-	) {
+	if (attribute === 'vscode-dark' || attribute === 'vscode-high-contrast') {
 		return 'vs-dark';
 	}
 
