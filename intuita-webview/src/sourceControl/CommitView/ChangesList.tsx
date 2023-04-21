6e267386ea4ee93ebe52dd34f00a2b2bcedf29cc
Benny Joo <sldisek783@gmail.com>
2023-04-21 13:57:10 +0100

    fix error

--- intuita-webview/src/sourceControl/CommitView/ChangesList.tsx
+++ intuita-webview/src/sourceControl/CommitView/ChangesList.tsx
@@ -23,6 +23,9 @@ const ChangesList = ({ formData, setFormData }: Props) => {
 
 	return (
 		<Tree
+			index={0}
+			isLastChild={false}
+			color="white"
 			renderItem={({ node, open, setIsOpen, depth }) => {
 				return (
 					<li
