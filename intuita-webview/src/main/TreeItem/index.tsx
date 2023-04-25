dd3ef96f89e74bd6d76a9624ab41ee1257b5db29
Benny Joo <sldisek783@gmail.com>
2023-04-25 14:45:30 +0100

    simplify ui

--- intuita-webview/src/main/TreeItem/index.tsx
+++ intuita-webview/src/main/TreeItem/index.tsx
@@ -27,78 +27,20 @@ const TreeItem = ({
 	focused,
 	actionButtons,
 	hasChildren,
-	kind,
 	onClick,
 	depth,
-	color,
-	index,
-	lastChild,
 }: Props) => {
-	const isCaseElement = kind === 'caseElement';
-	const isFolderElement = kind === 'folderElement';
-	const isCaseByFolderElement = kind === 'caseByFolderElement';
-	const isFirstSubfolder = index === 0 && depth > 1 && isFolderElement;
-
 	return (
 		<div
 			id={id}
 			className={cn(styles.root, focused && styles.focused)}
 			onClick={onClick}
 		>
-			{kind !== 'rootElement' && (
-				<div
-					className={styles.circleContainer}
-					style={{
-						...(depth > 1 && {
-							marginLeft: (depth - 1) * 2 * 16,
-						}),
-					}}
-				>
-					<div
-						className={cn(
-							isCaseByFolderElement && styles.smallCircle,
-							(isCaseElement || isFolderElement) &&
-								styles.bigCircle,
-						)}
-						style={{
-							...((isCaseElement || isFolderElement) && {
-								borderColor: color,
-							}),
-							...(isCaseByFolderElement && {
-								backgroundColor: color,
-							}),
-						}}
-					/>
-					{isCaseByFolderElement && (
-						<div
-							className={styles.verticalLine}
-							style={{
-								borderColor: color,
-								left: 11 + (depth - 1) * 2 * 16,
-								...(lastChild && {
-									height: 16,
-									marginTop: -10,
-								}),
-							}}
-						/>
-					)}
-					{isFirstSubfolder && (
-						<div
-							style={{
-								left: 11 + (depth - 2) * 2 * 16,
-								borderLeft: '2px solid',
-								borderBottom: '2px solid',
-								borderColor: color,
-								borderBottomLeftRadius: '5px',
-								height: 14,
-								width: 23,
-								position: 'absolute',
-								marginTop: -10,
-							}}
-						/>
-					)}
-				</div>
-			)}
+			<div
+				style={{
+					minWidth: depth * 16,
+				}}
+			/>
 			{hasChildren ? (
 				<div className={styles.codicon}>
 					<span
