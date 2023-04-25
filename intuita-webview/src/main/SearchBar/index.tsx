d7244297409a385cfbeba75aa78c662ed0bb4cfa
Benny Joo <sldisek783@gmail.com>
2023-04-25 17:23:25 +0100

    Build search bar component

---
+++ intuita-webview/src/main/SearchBar/index.tsx
@@ -0,0 +1,29 @@
+import { VSCodeTextField } from '@vscode/webview-ui-toolkit/react';
+import cn from 'classnames';
+import { Dispatch, SetStateAction } from 'react';
+import styles from './style.module.css';
+
+type Props = {
+	searchQuery: string;
+	setSearchQuery: Dispatch<SetStateAction<string>>;
+};
+
+export const SEARCH_QUERY_MIN_LENGTH = 3;
+
+const SearchBar = ({ searchQuery, setSearchQuery }: Props) => {
+	return (
+		<VSCodeTextField
+			type="text"
+			value={searchQuery}
+			placeholder=""
+			onInput={(event: any) => {
+				setSearchQuery((prev) => event.target?.value || prev);
+			}}
+			className={styles.container}
+		>
+			<span slot="start" className={cn('codicon', 'codicon-search')} />
+		</VSCodeTextField>
+	);
+};
+
+export default SearchBar;
