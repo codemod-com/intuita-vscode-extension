ada34c840f1ea384fb77edc8a76ac7eab4cf5156
Benny Joo <sldisek783@gmail.com>
2023-04-25 17:46:39 +0100

    remove lastChild and color props from Tree component

--- intuita-webview/src/shared/Tree/index.tsx
+++ intuita-webview/src/shared/Tree/index.tsx
@@ -1,7 +1,6 @@
 import ReactTreeView from 'react-treeview';
 import { ReactNode, memo, useState } from 'react';
 import { TreeNode } from '../../../../src/components/webview/webviewEvents';
-import { buildHash, generateColor } from '../../utilities';
 
 type Props = {
 	index: number;
@@ -13,38 +12,21 @@ type Props = {
 		depth,
 		open,
 		setIsOpen,
-		color,
-		lastChild,
 	}: {
 		index: number;
 		node: TreeNode;
 		depth: number;
 		open: boolean;
 		setIsOpen: (value: boolean) => void;
-		color: string;
-		lastChild: boolean;
 	}): ReactNode;
-	color: string;
-	lastChild: boolean;
 };
 
-const Tree = ({
-	node,
-	depth,
-	renderItem,
-	color: colorProp,
-	index,
-	lastChild,
-}: Props) => {
+const Tree = ({ node, depth, renderItem, index }: Props) => {
 	const hasNoChildren = !node.children || node.children.length === 0;
 	const [open, setIsOpen] = useState(true);
-	const [color] = useState(
-		hasNoChildren ? colorProp : generateColor(buildHash(node.id)),
-	);
+
 	const treeItem = renderItem({
 		index,
-		lastChild,
-		color,
 		node,
 		depth,
 		open,
@@ -59,15 +41,13 @@ const Tree = ({
 		<>
 			<ReactTreeView collapsed={!open} nodeLabel={treeItem}>
 				{open
-					? node.children.map((child, index, arr) => (
+					? node.children.map((child, index) => (
 							<Tree
 								key={child.id}
 								node={child}
 								renderItem={renderItem}
 								depth={depth + 1}
-								color={color}
 								index={index}
-								lastChild={arr.length - 1 === index}
 							/>
 					  ))
 					: null}
