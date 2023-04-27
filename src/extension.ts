d7e982ec4d715da1483e7fdc8ab664d1bf2d8170
Benny Joo <sldisek783@gmail.com>
2023-04-27 21:00:27 +0100

    fix build error

--- src/extension.ts
+++ src/extension.ts
@@ -357,7 +357,6 @@ export async function activate(context: vscode.ExtensionContext) {
 		messageBus,
 		jobManager,
 		caseManager,
-		sourceControl,
 	);
 
 	const intuitaFileExplorer = vscode.window.registerWebviewViewProvider(
