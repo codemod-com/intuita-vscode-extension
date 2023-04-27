bb4fee217b8f82e267590ab5bccdd77e57f07339
dhryshyn <dev.dmytroh@gmail.com>
2023-04-27 11:30:50 +0300

    fix labels

--- intuita-webview/src/jobDiffView/DiffViewer/Header/index.tsx
+++ intuita-webview/src/jobDiffView/DiffViewer/Header/index.tsx
@@ -39,7 +39,7 @@ const Header = ({ title, viewType, onViewChange }: Props) => {
 				</VSCodeButton>
 			) : (
 				<VSCodeButton
-					title="Split"
+					title="Side by Side"
 					appearance="icon"
 					onClick={() => onViewChange('side-by-side')}
 				>
