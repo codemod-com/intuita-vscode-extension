b7ecad67d74ab98a6e36a5b1b5ad3b2e3447dc8e
Benny Joo <sldisek783@gmail.com>
2023-05-03 11:10:17 +0100

    Replace Copy button with an icon

--- intuita-webview/src/jobDiffView/DiffViewer/Header/index.tsx
+++ intuita-webview/src/jobDiffView/DiffViewer/Header/index.tsx
@@ -1,6 +1,7 @@
 import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
 import { ReactComponent as UnifiedIcon } from '../../../assets/Unified.svg';
 import { ReactComponent as SplitIcon } from '../../../assets/Split.svg';
+import { ReactComponent as CopyIcon } from '../../../assets/copy.svg';
 import { DiffViewType, JobDiffViewProps } from '../../../shared/types';
 
 import styles from './style.module.css';
@@ -60,11 +61,17 @@ const Header = ({ title, viewType, diffId, jobs, onViewChange }: Props) => {
 			<div className={styles.title} onClick={handleTitleClick}>
 				<span>{title}</span>
 				<VSCodeButton
-					className={styles.copyButton}
 					onClick={handleTitleClick}
-					appearance="secondary"
+					appearance="icon"
+					className={styles.iconContainer}
 				>
-					Copy
+					<CopyIcon
+						className={styles.icon}
+						style={{
+							width: '21px',
+							height: '21px',
+						}}
+					/>
 				</VSCodeButton>
 			</div>
 			<div className={styles.actionsContainer}>
@@ -97,6 +104,7 @@ const Header = ({ title, viewType, diffId, jobs, onViewChange }: Props) => {
 					title="Inline"
 					appearance="icon"
 					onClick={() => onViewChange('inline')}
+					className={styles.iconContainer}
 				>
 					Inline <UnifiedIcon className={styles.icon} />
 				</VSCodeButton>
@@ -105,6 +113,7 @@ const Header = ({ title, viewType, diffId, jobs, onViewChange }: Props) => {
 					title="Side by Side"
 					appearance="icon"
 					onClick={() => onViewChange('side-by-side')}
+					className={styles.iconContainer}
 				>
 					Side by Side <SplitIcon className={styles.icon} />
 				</VSCodeButton>
