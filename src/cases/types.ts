eea95076d50e30dfea1d3e6f8e7fda2fa0df828a
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-05-04 17:37:45 +0200

    wip

--- src/cases/types.ts
+++ src/cases/types.ts
@@ -3,9 +3,7 @@ import type { JobHash } from '../jobs/types';
 export type CaseHash = string & { readonly __CaseHash: '__CaseHash' };
 
 export const enum CaseKind {
-	REWRITE_FILE_BY_POLYGLOT_PIRANHA = 1,
 	REWRITE_FILE_BY_NORA_NODE_ENGINE = 2,
-	REWRITE_FILE_BY_NORA_RUST_ENGINE = 3,
 }
 
 export type Case = Readonly<{
