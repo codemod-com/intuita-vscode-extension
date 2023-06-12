8bb3c05fd6a4f255d38da1ea250498351a81ce09
dhryshyn <dev.dmytroh@gmail.com>
2023-06-12 15:52:43 +0300

    wip

--- src/data/slice.ts
+++ src/data/slice.ts
@@ -1,27 +1,68 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { RootState } from './index';
+import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import { Case } from '../cases/types';
+import { Job } from '../jobs/types';
+import { CodemodEntry } from '../codemods/types';
 
 const SLICE_KEY = 'root';
 
+type CodemodDiscoveryState  = {
+	openedCodemodHashDigests: string;
+	focusedCodemodHashDigest: string;
+	executionPaths: Record<string, string>;
+	visible: boolean;
+}
+
+type CodemodRunsState  = {
+	selectedCaseHash: string;
+	visible: boolean;
+}
+
+type ChangeExplorerState = {
+	visible: boolean;
+}
+
+type CommunityState  = {
+	visible: boolean;
+}
+
+type State = Readonly<{
+	case: Record<string, Case>;
+	job: Record<string, Job>;
+	codemodDiscovery: CodemodDiscoveryState;
+	codemodRuns: CodemodRunsState;
+	changeExplorer: ChangeExplorerState;
+	community: CommunityState;
+}>;
+
+const codemodAdapter = createEntityAdapter<CodemodEntry>({
+  selectId: (codemod) => codemod.hashDigest
+});
+
+const caseAdapter = createEntityAdapter<Case>({
+	selectId: (kase) => kase.hash
+});
+
+const jobAdapter = createEntityAdapter<Job>({
+	selectId: (job) => job.hash,
+})
+
 const getInitialState = () => {
 	return {
-		test: '1',
+		codemod: codemodAdapter.getInitialState(), 
+		case: caseAdapter.getInitialState(), 
+		job: jobAdapter.getInitialState(), 
 	};
 };
 
 const rootSlice = createSlice({
 	name: SLICE_KEY,
 	initialState: getInitialState(),
-	reducers: {
-		testAction(state, action: PayloadAction<string>) {
-			state.test = action.payload;
-		},
-	},
+	reducers: {},
 });
 
-const { testAction } = rootSlice.actions;
-const selector = (state: RootState) => state;
+const actions = rootSlice.actions;
+const selector = (state: State) => state;
 
-export { testAction, selector, SLICE_KEY };
+export { actions, selector, SLICE_KEY };
 
 export default rootSlice.reducer;
