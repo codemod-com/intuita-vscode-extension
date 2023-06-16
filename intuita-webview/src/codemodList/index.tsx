fc30570470b25757bd9a08db0d4ad702ccf1aa85
dhryshyn <dev.dmytroh@gmail.com>
2023-06-16 10:01:22 +0300

    fix

--- intuita-webview/src/codemodList/index.tsx
+++ intuita-webview/src/codemodList/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../shared/index.css';
 import App from './App';
+import '../shared/index.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
