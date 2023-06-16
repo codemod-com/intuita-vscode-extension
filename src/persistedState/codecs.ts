ac346ca83f35fb0f99e9c33b5bc7a93c2ab5a07a
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-06-16 11:12:41 +0200

    INT-1200 Replace the Persisted State with Redux (#524)

--- src/persistedState/codecs.ts
+++ src/persistedState/codecs.ts
@@ -1,48 +1,5 @@
 import * as t from 'io-ts';
-import { withFallback } from 'io-ts-types/lib/withFallback';
-import { CaseKind } from '../cases/types';
-import { JobKind } from '../jobs/types';
 import { buildTypeCodec } from '../utilities';
-
-export const persistedJobCodec = buildTypeCodec({
-	kind: t.union([
-		t.literal(JobKind.rewriteFile),
-		t.literal(JobKind.createFile),
-		t.literal(JobKind.deleteFile),
-		t.literal(JobKind.moveAndRewriteFile),
-		t.literal(JobKind.moveFile),
-		t.literal(JobKind.copyFile),
-	]),
-	oldPath: t.union([t.string, t.null]),
-	newPath: t.union([t.string, t.null]),
-	oldContentPath: t.union([t.string, t.null]),
-	newContentPath: t.union([t.string, t.null]),
-	hash: t.string,
-	codemodSetName: t.string,
-	codemodName: t.string,
-	executionId: t.string,
-});
-
-export type PersistedJob = t.TypeOf<typeof persistedJobCodec>;
-
-export const persistedCaseCodec = buildTypeCodec({
-	kind: t.literal(CaseKind.REWRITE_FILE_BY_NORA_NODE_ENGINE),
-	subKind: t.string,
-	hash: t.string,
-	codemodSetName: t.string,
-	codemodName: t.string,
-});
-
-export type PersistedCase = t.TypeOf<typeof persistedCaseCodec>;
-
-export const persistedStateCodec = buildTypeCodec({
-	cases: t.readonlyArray(persistedCaseCodec),
-	jobs: t.readonlyArray(persistedJobCodec),
-	caseHashJobHashes: t.readonlyArray(t.string),
-	appliedJobsHashes: withFallback(t.readonlyArray(t.string), []),
-});
-
-export type PersistedState = t.TypeOf<typeof persistedStateCodec>;
 
 export const syntheticErrorCodec = buildTypeCodec({
 	kind: t.literal('syntheticError'),
