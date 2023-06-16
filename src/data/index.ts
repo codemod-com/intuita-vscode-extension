9f8afce855cd7d967a902834106630157049c69f
dhryshyn <dev.dmytroh@gmail.com>
2023-06-16 12:21:04 +0300

    validation

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,14 @@
-import { configureStore, Dispatch } from '@reduxjs/toolkit';
+import { configureStore, Dispatch, Reducer } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import MementoStorage from './storage';
 
 import rootReducer, { actions } from './slice';
 import { Memento } from 'vscode';
+import prettyReporter from 'io-ts-reporters';
+import { PersistPartial } from 'redux-persist/es/persistReducer';
+import { persistedStateCodecNew } from '../persistedState/codecs';
+
+
 
 const buildStore = (workspaceState: Memento) => {
 	const persistedReducer = persistReducer(
@@ -14,10 +19,27 @@ const buildStore = (workspaceState: Memento) => {
 		},
 		rootReducer,
 	);
+	
+	
+	const validatedReducer: Reducer<
+		(RootState & PersistPartial) | undefined
+	> = (state, action) => {
+		if (action.type === 'persist/REHYDRATE') {
+			const decoded = persistedStateCodecNew.decode(action.payload);
 
+			return persistedReducer(state, {
+				type: action.type,
+				payload: decoded,
+			});
+		}
+
+		return persistedReducer(state, action);
+	};
+	
 	const store = configureStore({
-		reducer: persistedReducer,
+		reducer: validatedReducer,
 	});
+	
 
 	const persistor = persistStore(store);
 
