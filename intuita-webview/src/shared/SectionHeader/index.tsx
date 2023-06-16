fb71111fe667f95234a67ffbfd17d44df480e08e
Benny Joo <sldisek783@gmail.com>
2023-06-16 05:39:33 -0400

    Fix chevron not working in Panel Section Header (#526)

--- intuita-webview/src/shared/SectionHeader/index.tsx
+++ intuita-webview/src/shared/SectionHeader/index.tsx
@@ -1,12 +1,12 @@
 import cn from 'classnames';
 import s from './style.module.css';
 import { Command } from 'vscode';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { vscode } from '../utilities/vscode';
 
 type Props = {
 	title: string;
-	open: boolean;
+	defaultOpen: boolean;
 	commands: ReadonlyArray<Command & { icon: string }>;
 	children?: ReactNode;
 	onHeaderClick?(): void;
@@ -19,9 +19,21 @@ const handleCommand = (c: Command) => {
 	});
 };
 
-const SectionHeader = ({ open, title, commands, onHeaderClick }: Props) => {
+const SectionHeader = ({
+	defaultOpen,
+	title,
+	commands,
+	onHeaderClick,
+}: Props) => {
+	const [open, setOpen] = useState(defaultOpen);
 	return (
-		<div className={s.sectionHeader} onClick={onHeaderClick}>
+		<div
+			className={s.sectionHeader}
+			onClick={() => {
+				onHeaderClick?.();
+				setOpen((prev) => !prev);
+			}}
+		>
 			<span
 				className={cn(
 					s.icon,
