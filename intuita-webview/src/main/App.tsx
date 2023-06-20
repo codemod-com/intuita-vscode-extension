5bb3ba5a4e0f5a964c9dc6489e8dcae6af3bf5bb
DmytroHryshyn <125881252+DmytroHryshyn@users.noreply.github.com>
2023-06-20 19:20:59 +0300

    Dmytro/int 1217 left panel redesign (#548)
    * wip
    
    * fix styles
    
    * fix
    
    * fix
    
    * fix

--- intuita-webview/src/main/App.tsx
+++ intuita-webview/src/main/App.tsx
@@ -1,100 +1,18 @@
-import {
-	PanelResizeHandle,
-	ImperativePanelHandle,
-} from 'react-resizable-panels';
-import CampaignManager from '../campaignManager/App';
-import CodemodList from '../codemodList/App';
-import FileExplorer from '../fileExplorer/App';
-import CommunityView from '../communityView/App';
-import { ResizablePanel, PanelGroup } from '../shared/Panel';
-import {
-	CSSProperties,
-	ReactElement,
-	useEffect,
-	useRef,
-	useState,
-} from 'react';
-import SectionHeader from '../shared/SectionHeader';
-import { WebviewMessage } from '../shared/types';
-import { CollapsibleWebviews } from '../../../src/components/webview/webviewEvents';
+import { useEffect, useRef, useState } from 'react';
 
-const PANELS: {
-	id: CollapsibleWebviews;
-	title: string;
-	Component: (props: any) => ReactElement | null;
-	commands?: any[];
-	inlineStyle?: CSSProperties;
-}[] = [
-	{
-		id: 'codemodDiscoveryView',
-		title: 'Codemod Discovery',
-		Component: (props) => CodemodList(props),
-	},
-	{
-		id: 'codemodRunsView',
-		title: 'Codemod Runs',
-		commands: [
-			{
-				icon: 'clear-all',
-				title: 'Clear all',
-				command: 'intuita.clearState',
-			},
-		],
-		Component: (props) => CampaignManager(props),
-	},
-	{
-		id: 'changeExplorerView',
-		title: 'Changes Explorer',
-		Component: (props) => FileExplorer(props),
-	},
-	{
-		id: 'communityView',
-		title: 'Community',
-		Component: (props) => CommunityView(props),
-		inlineStyle: { flex: 'auto' },
-	},
-];
+import {
+	VSCodePanels,
+	VSCodePanelTab,
+	VSCodePanelView,
+} from '@vscode/webview-ui-toolkit/react';
+
+import CodemodList from '../codemodList/App';
+import CommunityView from '../communityView/App';
+import CodemodRuns from './CodemodRuns';
 
 function App() {
-	const panelRefs = useRef<Record<string, ImperativePanelHandle>>({});
 	const ref = useRef(null);
 	const [screenWidth, setScreenWidth] = useState<number | null>(null);
-	const togglePanel = (id: string) => {
-		const ref = panelRefs.current[id];
-
-		if (!ref) {
-			return;
-		}
-		const collapsed = ref.getCollapsed();
-		if (collapsed) {
-			ref.expand();
-		} else {
-			ref.collapse();
-		}
-	};
-
-	useEffect(() => {
-		const handler = (e: MessageEvent<WebviewMessage>) => {
-			const message = e.data;
-			if (message.kind === 'webview.main.setCollapsed') {
-				const ref = panelRefs.current[message.viewName];
-				if (!ref) {
-					return;
-				}
-				if (message.collapsed) {
-					ref.collapse();
-				} else {
-					ref.expand();
-				}
-			}
-		};
-
-		window.addEventListener('message', handler);
-
-		return () => {
-			window.removeEventListener('message', handler);
-		};
-	}, []);
 
 	useEffect(() => {
 		if (ResizeObserver === undefined) {
@@ -126,53 +44,26 @@ function App() {
 
 	return (
 		<main className="App" ref={ref}>
-			<PanelGroup direction="vertical">
-				{PANELS.map(
-					(
-						{
-							id,
-							title,
-							commands = [],
-							Component,
-							inlineStyle = {},
-						},
-						idx,
-					) => {
-						const collapsed = panelRefs.current[id]?.getCollapsed();
-
-						return (
-							<>
-								{idx !== 0 ? (
-									<PanelResizeHandle className="resize-handle" />
-								) : null}
-								<SectionHeader
-									title={title}
-									commands={commands}
-									defaultOpen={!collapsed}
-									onHeaderClick={() => togglePanel(id)}
-								/>
-								<ResizablePanel
-									collapsible
-									minSize={0}
-									defaultSize={25}
-									ref={(ref) => {
-										if (ref) {
-											panelRefs.current[id] = ref;
-										}
-									}}
-									style={{
-										overflowY: 'auto',
-										overflowX: 'hidden',
-										...inlineStyle,
-									}}
-								>
-									<Component screenWidth={screenWidth} />
-								</ResizablePanel>
-							</>
-						);
-					},
-				)}
-			</PanelGroup>
+			<VSCodePanels className="h-full w-full vscode-panels">
+				<VSCodePanelTab className="vscode-tab" id="tab-1">
+					Codemods Discovery
+				</VSCodePanelTab>
+				<VSCodePanelTab className="vscode-tab" id="tab-2">
+					Codemod Runs
+				</VSCodePanelTab>
+				<VSCodePanelTab className="vscode-tab" id="tab-3">
+					Community
+				</VSCodePanelTab>
+				<VSCodePanelView className="h-full w-full">
+					<CodemodList screenWidth={screenWidth} />
+				</VSCodePanelView>
+				<VSCodePanelView className="h-full w-full">
+					<CodemodRuns screenWidth={screenWidth} />
+				</VSCodePanelView>
+				<VSCodePanelView className="h-full w-full">
+					<CommunityView screenWidth={screenWidth} />
+				</VSCodePanelView>
+			</VSCodePanels>
 		</main>
 	);
 }
---
+++ intuita-webview/src/main/CodemodRuns.tsx
@@ -0,0 +1,140 @@
+import { CSSProperties, useEffect, useRef } from 'react';
+
+import {
+	PanelResizeHandle,
+	ImperativePanelHandle,
+} from 'react-resizable-panels';
+
+import { ResizablePanel, PanelGroup } from '../shared/Panel';
+import SectionHeader from '../shared/SectionHeader';
+import { WebviewMessage } from '../shared/types';
+
+import CampaignManager from '../campaignManager/App';
+import FileExplorer from '../fileExplorer/App';
+
+import { CollapsibleWebviews } from '../../../src/components/webview/webviewEvents';
+
+type Props = Readonly<{
+	screenWidth: number | null;
+}>;
+
+const RESIZABLE_PANELS: {
+	id: CollapsibleWebviews;
+	title: string;
+	Component: React.FC<any>;
+	commands?: any[];
+	inlineStyle?: CSSProperties;
+}[] = [
+	{
+		id: 'codemodRunsView',
+		title: 'Codemod Runs',
+		commands: [
+			{
+				icon: 'clear-all',
+				title: 'Clear all',
+				command: 'intuita.clearState',
+			},
+		],
+		Component: (props) => CampaignManager(props),
+	},
+	{
+		id: 'changeExplorerView',
+		title: 'Changes Explorer',
+		Component: (props) => FileExplorer(props),
+	},
+];
+
+const CodemodRuns = ({ screenWidth }: Props) => {
+	const panelRefs = useRef<Record<string, ImperativePanelHandle>>({});
+
+	const togglePanel = (id: string) => {
+		const ref = panelRefs.current[id];
+
+		if (!ref) {
+			return;
+		}
+		const collapsed = ref.getCollapsed();
+		if (collapsed) {
+			ref.expand();
+		} else {
+			ref.collapse();
+		}
+	};
+
+	useEffect(() => {
+		const handler = (e: MessageEvent<WebviewMessage>) => {
+			const message = e.data;
+			if (message.kind === 'webview.main.setCollapsed') {
+				const ref = panelRefs.current[message.viewName];
+				if (!ref) {
+					return;
+				}
+				if (message.collapsed) {
+					ref.collapse();
+				} else {
+					ref.expand();
+				}
+			}
+		};
+
+		window.addEventListener('message', handler);
+
+		return () => {
+			window.removeEventListener('message', handler);
+		};
+	}, []);
+
+	return (
+		<div className="w-full h-full">
+			<PanelGroup direction="vertical">
+				{RESIZABLE_PANELS.map(
+					(
+						{
+							id,
+							title,
+							commands = [],
+							Component,
+							inlineStyle = {},
+						},
+						idx,
+					) => {
+						const collapsed = panelRefs.current[id]?.getCollapsed();
+
+						return (
+							<>
+								{idx !== 0 ? (
+									<PanelResizeHandle className="resize-handle" />
+								) : null}
+								<SectionHeader
+									title={title}
+									commands={commands}
+									defaultOpen={!collapsed}
+									onHeaderClick={() => togglePanel(id)}
+								/>
+								<ResizablePanel
+									collapsible
+									minSize={0}
+									defaultSize={50}
+									ref={(ref) => {
+										if (ref) {
+											panelRefs.current[id] = ref;
+										}
+									}}
+									style={{
+										overflowY: 'auto',
+										overflowX: 'hidden',
+										...inlineStyle,
+									}}
+								>
+									<Component screenWidth={screenWidth} />
+								</ResizablePanel>
+							</>
+						);
+					},
+				)}
+			</PanelGroup>
+		</div>
+	);
+};
+
+export default CodemodRuns;
