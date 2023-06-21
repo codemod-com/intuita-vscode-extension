dd04717124b0914cae74d8d44fb7627f204abda5
Benny Joo <sldisek783@gmail.com>
2023-06-21 07:22:25 -0400

    Store/Use Panel Tab id properly

--- src/components/webview/webviewEvents.ts
+++ src/components/webview/webviewEvents.ts
@@ -305,6 +305,10 @@ export type WebviewResponse =
 			id: TreeNodeId;
 	  }>
 	| Readonly<{
+			kind: 'webview.main.setState';
+			activeTabId: TabKind;
+	  }>
+	| Readonly<{
 			kind: 'webview.codemodList.updatePathToExecute';
 			value: {
 				newPath: string;
