c4490c0e8ddfca4d09c7da4c12d7156351f0e74c
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-06-22 11:27:29 +0200

    wip

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,13 +3,11 @@ import * as vscode from 'vscode';
 export const getConfiguration = () => {
 	const configuration = vscode.workspace.getConfiguration('intuita');
 
-	const saveDocumentOnJobAccept =
-		configuration.get<boolean>('saveDocumentOnJobAccept') ?? true;
-
 	const fileLimit = configuration.get<number>('fileLimit') ?? 100;
 
 	const workerThreadCount =
 		configuration.get<number>('workerThreadCount') ?? 4;
+
 	const includePatterns = configuration.get<string[]>('includePatterns') ?? [
 		'**/*.{js,ts,jsx,tsx,cjs,mjs}',
 	];
@@ -21,7 +19,6 @@ export const getConfiguration = () => {
 		configuration.get<boolean>('formatWithPrettier') ?? false;
 
 	return {
-		saveDocumentOnJobAccept,
 		fileLimit,
 		workerThreadCount,
 		includePatterns,
