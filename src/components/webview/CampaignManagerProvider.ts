71a150d1be7dd8db53d2a80cb2391f0ccdfbf26e
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-06-26 12:26:54 +0200

    INT-1241 refactor the providers (#608)

--- src/components/webview/CampaignManagerProvider.ts
+++ src/components/webview/CampaignManagerProvider.ts
@@ -1,6 +1,5 @@
-import { WebviewView, commands } from 'vscode';
+import { WebviewView } from 'vscode';
 import { View, WebviewMessage } from './webviewEvents';
-import { actions } from '../../data/slice';
 import { Store } from '../../data';
 import areEqual from 'fast-deep-equal';
 import { selectCodemodRunsTree } from '../../selectors/selectCodemodRunsTree';
@@ -10,35 +9,7 @@ type ViewProps = Extract<View, { viewId: 'campaignManagerView' }>['viewProps'];
 export class CampaignManager {
 	private __webviewView: WebviewView | null = null;
 
-	constructor(private readonly __store: Store) {}
-
-	setWebview(webviewView: WebviewView): void | Thenable<void> {
-		this.__webviewView = webviewView;
-		this.__attachExtensionEventListeners();
-		this.__attachWebviewEventListeners();
-	}
-
-	public setView() {
-		const viewProps = this.__buildViewProps();
-
-		this.__postMessage({
-			kind: 'webview.codemodRuns.setView',
-			value: {
-				viewId: 'campaignManagerView',
-				viewProps,
-			},
-		});
-	}
-
-	public showView() {
-		this.__webviewView?.show(true);
-	}
-
-	private __postMessage(message: WebviewMessage) {
-		this.__webviewView?.webview.postMessage(message);
-	}
-
-	private __attachExtensionEventListeners() {
+	constructor(private readonly __store: Store) {
 		let prevProps = this.__buildViewProps();
 
 		this.__store.subscribe(() => {
@@ -60,31 +31,16 @@ export class CampaignManager {
 		});
 	}
 
-	private __attachWebviewEventListeners() {
-		if (this.__webviewView === null) {
-			return;
-		}
-
-		this.__webviewView.webview.onDidReceiveMessage((message) => {
-			if (message.kind === 'webview.command') {
-				commands.executeCommand(
-					message.value.command,
-					...(message.value.arguments ?? []),
-				);
-			}
-
-			if (
-				message.kind === 'webview.campaignManager.setSelectedCaseHash'
-			) {
-				this.__store.dispatch(
-					actions.setSelectedCaseHash(message.caseHash),
-				);
-			}
-		});
-	}
-
 	public getInitialProps(): ViewProps {
 		return this.__buildViewProps();
+	}
+
+	public setWebview(webviewView: WebviewView): void | Thenable<void> {
+		this.__webviewView = webviewView;
+	}
+
+	private __postMessage(message: WebviewMessage) {
+		this.__webviewView?.webview.postMessage(message);
 	}
 
 	private __buildViewProps(): ViewProps {
