1c6c3b484ead0de44aa55c82e0fdc92246f0fd1d
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-06-27 12:07:38 +0200

    INT-1246 implement selectMainWebviewViewProps (#623)

---
+++ src/selectors/selectMainWebviewViewProps.ts
@@ -0,0 +1,37 @@
+import type { Uri } from 'vscode';
+import type { RootState } from '../data';
+import { TabKind } from '../persistedState/codecs';
+import { selectCodemodRunsTree } from './selectCodemodRunsTree';
+import { selectCodemodTree } from './selectCodemodTree';
+import { selectExplorerTree } from './selectExplorerTree';
+
+export const selectMainWebviewViewProps = (
+	state: RootState,
+	rootUri: Uri,
+	autocompleteItems: ReadonlyArray<string>,
+) => {
+	if (state.activeTabId === TabKind.codemods) {
+		return {
+			activeTabId: state.activeTabId,
+			searchPhrase: state.codemodDiscoveryView.searchPhrase,
+			autocompleteItems,
+			codemodTree: selectCodemodTree(state, rootUri.fsPath),
+		};
+	}
+
+	if (state.activeTabId === TabKind.codemodRuns) {
+		return {
+			activeTabId: state.activeTabId,
+			codemodRunsTree: selectCodemodRunsTree(state),
+			changeExplorerTree: selectExplorerTree(state),
+		};
+	}
+
+	return {
+		activeTabId: state.activeTabId,
+	};
+};
+
+export type MainWebviewViewProps = ReturnType<
+	typeof selectMainWebviewViewProps
+>;
