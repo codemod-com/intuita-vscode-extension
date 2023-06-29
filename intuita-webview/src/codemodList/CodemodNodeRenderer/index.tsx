dba13da11e797e40e7196f77921db7b4d2b93ed3
Benny Joo <sldisek783@gmail.com>
2023-06-29 12:40:15 -0400

    Remove the short description in popover of codemod item (#654)

--- intuita-webview/src/codemodList/CodemodNodeRenderer/index.tsx
+++ intuita-webview/src/codemodList/CodemodNodeRenderer/index.tsx
@@ -73,7 +73,6 @@ const getCodemodNodeRenderer =
 				{node.kind === 'CODEMOD' && (
 					<Codemod
 						hashDigest={hashDigest}
-						description={node.description}
 						executionPath={node.executionPath}
 						label={label}
 						autocompleteItems={autocompleteItems}
