84df5433e2d1c06081e0d4a48bde8488079a8753
Benny Joo <sldisek783@gmail.com>
2023-06-30 10:07:24 -0400

    prettier

--- intuita-webview/src/shared/IntuitaPopover/index.tsx
+++ intuita-webview/src/shared/IntuitaPopover/index.tsx
@@ -2,18 +2,8 @@ import Tippy, { TippyProps } from '@tippyjs/react';
 
 type Props = TippyProps;
 
-const IntuitaPopover = ({
-	delay = 200,
-	...others
-}: Props) => {
-	return (
-		<Tippy
-			arrow
-			delay={delay}
-			placement="auto"
-			{...others}
-		/>
-	);
+const IntuitaPopover = ({ delay = 200, ...others }: Props) => {
+	return <Tippy arrow delay={delay} placement="auto" {...others} />;
 };
 
 export default IntuitaPopover;
