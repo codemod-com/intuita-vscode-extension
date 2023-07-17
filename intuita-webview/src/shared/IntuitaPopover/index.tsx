e87de5f4d066d1636c57e5945f015da1446852af
Benny Joo <sldisek783@gmail.com>
2023-07-17 09:28:18 -0400

    Trigger Intuita Popover on hover only, not focus

--- intuita-webview/src/shared/IntuitaPopover/index.tsx
+++ intuita-webview/src/shared/IntuitaPopover/index.tsx
@@ -3,7 +3,15 @@ import Tippy, { TippyProps } from '@tippyjs/react';
 type Props = TippyProps;
 
 const IntuitaPopover = ({ delay = [800, 100], ...others }: Props) => {
-	return <Tippy arrow delay={delay} placement="auto" {...others} />;
+	return (
+		<Tippy
+			trigger="mouseenter"
+			arrow
+			delay={delay}
+			placement="auto"
+			{...others}
+		/>
+	);
 };
 
 export default IntuitaPopover;
