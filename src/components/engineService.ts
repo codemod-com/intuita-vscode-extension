263f9543a1861da94c24e97891f8712699ca6d14
Benny Joo <sldisek783@gmail.com>
2024-01-02 13:26:24 +0000

    INT-2369 Do not display "No change was made from codemod" if codemod was halted

--- src/components/engineService.ts
+++ src/components/engineService.ts
@@ -838,8 +838,11 @@ export class EngineService {
 				);
 
 				commands.executeCommand('intuitaMainView.focus');
-
-				if (!this.#execution.affectedAnyFile) {
+				
+				if (
+					!this.#execution.halted &&
+					!this.#execution.affectedAnyFile
+				) {
 					window.showWarningMessage(Messages.noAffectedFiles);
 				}
 			}
